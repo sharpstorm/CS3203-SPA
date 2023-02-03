55043b93acc14494e99ed3dea16d024e0ea70f8b
lingshanng <nglingshan456@gmail.com>
2023-02-03 10:23:13 +0800

    feat: add PkbWriter

--- Team30/Code30/src/spa/src/pkb/writers/PkbWriter.h
+++ Team30/Code30/src/spa/src/pkb/writers/PkbWriter.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include "../../common/Types.h"
+#include "../storage/PKB.h"
+#include "FollowsWriter.h"
+#include "ParentWriter.h"
 
 class PkbWriter {
-public:
-	PkbWriter(PKB& pkb);
+ public:
+  explicit PkbWriter(PKB* pkb);
 
-	void addFollows(int arg1, int arg2);
-	void addParent(int arg1, int arg2);
+  void addFollows(int arg1, int arg2);
+  void addParent(int arg1, int arg2);
+
+ private:
+  FollowsWriter followsWriter;
+  ParentWriter parentWriter;
 };
