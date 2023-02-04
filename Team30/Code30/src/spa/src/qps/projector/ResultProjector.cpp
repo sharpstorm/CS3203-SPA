c54de847d7faf955cd6de07ec410daf50ec38d13
KwanHW <kwanhaowei@gmail.com>
2023-02-04 21:12:09 +0800

    feat: add projecting logic

--- Team30/Code30/src/spa/src/qps/projector/ResultProjector.cpp
+++ Team30/Code30/src/spa/src/qps/projector/ResultProjector.cpp
@@ -8,16 +8,32 @@ string *ResultProjector::project(QueryResult* queryResult) {
     }
 
     string result;
-    if (queryResult->getEntityList().size() == 0) {
-        // Convert statement list to a list of strings
-        for (int stmt : queryResult->getStatementList()) {
-            result += to_string(stmt);
-        }
-    } else {
-        for (string &entity : queryResult->getEntityList()) {
-            result += entity;
-        }
+    if (queryResult->isEntityMapEmpty()) {
+      STATEMENT_MAP statementMap = queryResult->getStatementMap();
+      projectStatements(&result, statementMap);
+    } else if (queryResult->isStatementMapEmpty()) {
+      ENTITY_MAP entityMap = queryResult->getEntityMap();
+      projectEntities(&result, entityMap);
     }
 
     return &result;
+}
+
+void ResultProjector::projectStatements(string *result,
+                                        STATEMENT_MAP statementMap) {
+  for (auto it=statementMap.begin(); it != statementMap.end(); ++it) {
+    StatementResult statementResult = it->second;
+    for (int stmt : statementResult.lines) {
+      *result += to_string(stmt);
+    }
+  }
+}
+
+void ResultProjector::projectEntities(string *result, ENTITY_MAP entityMap) {
+  for (auto it=entityMap.begin(); it != entityMap.end(); ++it)  {
+    EntityResult entityResult = it->second;
+    for (string &entity : entityResult.entities) {
+      *result += entity;
+    }
+  }
 }
--- Team30/Code30/src/spa/src/qps/projector/ResultProjector.h
+++ Team30/Code30/src/spa/src/qps/projector/ResultProjector.h
@@ -8,4 +8,6 @@ using std::string;
 class ResultProjector {
  public:
   string* project(QueryResult* queryResult);
+  void projectEntities(string* result, ENTITY_MAP entityMap);
+  void projectStatements(string* result, STATEMENT_MAP statementMap);
 };
