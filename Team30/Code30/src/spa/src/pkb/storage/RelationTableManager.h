c7f4333a4e201ac4d8daabc7f6ba31be267180ca
lingshanng <nglingshan456@gmail.com>
2023-02-04 22:06:17 +0800

    feat: add RelationTableManager methods and tests

--- Team30/Code30/src/spa/src/pkb/storage/RelationTableManager.h
+++ Team30/Code30/src/spa/src/pkb/storage/RelationTableManager.h
@@ -4,6 +4,7 @@
 #include <unordered_set>
 
 #include "../../common/Types.h"
+#include "../predicates/Predicate.h"
 #include "tables/BaseTable.h"
 
 using std::shared_ptr;
@@ -12,35 +13,56 @@ using std::unordered_set;
 template <typename K, typename V>
 class RelationTableManager {
  private:
-  BaseTable<K, V>* table;         // maps T -> set<U>
-  BaseTable<V, K>* reverseTable;  // maps U -> set<T>
+  shared_ptr<BaseTable<K, V>> table;         // maps K -> set<V>
+  shared_ptr<BaseTable<V, K>> reverseTable;  // maps V -> set<K>
 
  public:
-  RelationTableManager(BaseTable<K, V>* table, BaseTable<V, K>* reverseTable);
+  RelationTableManager(shared_ptr<BaseTable<K, V>> table,
+                       shared_ptr<BaseTable<V, K>> reverseTable)
+      : table(table), reverseTable(reverseTable) {}
 
-  void insert(K arg1, V arg2);
+  void insert(K arg1, V arg2) {
+    table->set(arg1, arg2);
+    reverseTable->set(arg2, arg1);
+  }
 
-  unordered_set<V> getByArg1(K arg1);
+  unordered_set<V> getByFirstArg(K arg1) { return table->get(arg1); }
 
-  unordered_set<K> getByArg2(V arg2);
-  // QueryResult<K, V>* query(unordered_set<T> key1Set,
-  //                         Predicate<U> key2Predicate) {
-  //  QueryResult<K, V>* result = new QueryResult<K, V>();
+  unordered_set<K> getBySecondArg(V arg2) { return reverseTable->get(arg2); }
 
-  //  for (key1 : key1Set) {
-  //    U keys2 = table.get(key1);
-  //    for (key2 : keys2) {
-  //      if (key2Predicate.test(key2)) {
-  //        result.add(key1, key2);
-  //      }
-  //    }
-  //  }
-  //  return result;
-  //}
-  // QueryResult<K, V>* query(T arg1, Predicate<U> arg2Predicate) {
-  //  return query(unordered_set<T>({arg1}))
-  //}
+  QueryResult<K, V> query(unordered_set<K> arg1Values,
+                          Predicate<V> arg2Predicate) {
+    QueryResult<K, V> result;
+    for (auto arg1 : arg1Values) {
+      auto arg2Values = table->get(arg1);
+      for (auto arg2 : arg2Values) {
+        if (arg2Predicate(arg2)) {
+          result.add(arg1, arg2);
+        }
+      }
+    }
+    return result;
+  }
 
-  // getByKey(T arg1)
-  // getByValue(T arg1)
+  QueryResult<K, V> query(Predicate<K> arg1Predicate,
+                          unordered_set<V> arg2Values) {
+    QueryResult<K, V> result;
+    for (auto arg2 : arg2Values) {
+      auto arg1Values = reverseTable->get(arg2);
+      for (auto arg1 : arg1Values) {
+        if (arg1Predicate(arg1)) {
+          result.add(arg1, arg2);
+        }
+      }
+    }
+    return result;
+  }
+
+  QueryResult<K, V> query(K arg1, Predicate<V> arg2Predicate) {
+    return query(unordered_set<K>({arg1}), arg2Predicate);
+  }
+
+  QueryResult<K, V> query(Predicate<K> arg1Predicate, V arg2) {
+    return query(arg1Predicate, unordered_set<V>({arg2}));
+  }
 };
---
+++ Team30/Code30/src/unit_testing/src/pkb/storage/RelationTableManagerTest.cpp
@@ -0,0 +1,133 @@
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <utility>
+
+#include "catch.hpp"
+#include "pkb/predicates/Predicate.h"
+#include "pkb/predicates/PredicateFactory.h"
+#include "pkb/storage/RelationTableManager.h"
+#include "pkb/storage/StructureMappingProvider.h"
+#include "pkb/storage/tables/ContiguousTable.h"
+#include "pkb/storage/tables/HashKeyTable.h"
+
+using std::make_shared;
+using std::string;
+using std::unordered_set;
+
+TEST_CASE("RelationTableManager getByArg1 getByArg2") {
+  RelationTableManager<int, string> tableManager(
+      make_shared<ContiguousTable<string>>(),
+      make_shared<HashKeyTable<string, int>>());
+
+  tableManager.insert(1, "a");
+  tableManager.insert(2, "b");
+  tableManager.insert(1, "c");
+  tableManager.insert(4, "b");
+
+  REQUIRE(tableManager.getByFirstArg(1) == unordered_set<string>({"a", "c"}));
+  REQUIRE(tableManager.getBySecondArg("b") == unordered_set<int>({2, 4}));
+}
+
+TEST_CASE("RelationTableManager known arg1 values with arg2 predicate") {
+  RelationTableManager<int, string> tableManager(
+      make_shared<ContiguousTable<string>>(),
+      make_shared<HashKeyTable<string, int>>());
+
+  tableManager.insert(1, "a");
+  tableManager.insert(2, "b");
+  tableManager.insert(1, "c");
+  tableManager.insert(4, "e");
+  tableManager.insert(3, "f");
+
+  unordered_set<string> validValues({"a", "e", "f"});
+  Predicate<string> isValid = [validValues](string const &s) {
+    return validValues.find(s) != validValues.end();
+  };
+
+  auto res = tableManager.query(unordered_set<int>({1, 2, 4}), isValid);
+
+  REQUIRE(res.firstArgVals == unordered_set<int>({1, 4}));
+  REQUIRE(res.secondArgVals == unordered_set<string>({"a", "e"}));
+
+  unordered_set<std::pair<int, string>> pairs;
+  pairs.insert(std::make_pair(1, "a"));
+  pairs.insert(std::make_pair(4, "e"));
+  REQUIRE(res.pairVals == pairs);
+}
+
+TEST_CASE("RelationTableManager known arg2 values with arg1 predicate") {
+  RelationTableManager<int, string> tableManager(
+      make_shared<ContiguousTable<string>>(),
+      make_shared<HashKeyTable<string, int>>());
+
+  tableManager.insert(1, "a");
+  tableManager.insert(2, "b");
+  tableManager.insert(4, "a");
+  tableManager.insert(2, "c");
+  tableManager.insert(3, "f");
+
+  unordered_set<int> validValues({2, 4, 5});
+  Predicate<int> isValid = [validValues](int const &s) {
+    return validValues.find(s) != validValues.end();
+  };
+
+  auto res =
+      tableManager.query(isValid, unordered_set<string>({"a", "b", "f"}));
+
+  REQUIRE(res.firstArgVals == unordered_set<int>({2, 4}));
+  REQUIRE(res.secondArgVals == unordered_set<string>({"a", "b"}));
+
+  unordered_set<std::pair<int, string>> pairs;
+  pairs.insert(std::make_pair(4, "a"));
+  pairs.insert(std::make_pair(2, "b"));
+  REQUIRE(res.pairVals == pairs);
+}
+
+TEST_CASE("RelationTableManager known arg1 with arg2 predicate") {
+  RelationTableManager<int, string> tableManager(
+      make_shared<ContiguousTable<string>>(),
+      make_shared<HashKeyTable<string, int>>());
+
+  tableManager.insert(1, "a");
+  tableManager.insert(2, "b");
+  tableManager.insert(2, "c");
+
+  unordered_set<string> validValues({"a", "b"});
+  Predicate<string> isValid = [validValues](string const &s) {
+    return validValues.find(s) != validValues.end();
+  };
+  auto res = tableManager.query(2, isValid);
+
+  REQUIRE(res.firstArgVals == unordered_set<int>({2}));
+  REQUIRE(res.secondArgVals == unordered_set<string>({"b"}));
+
+  unordered_set<std::pair<int, string>> pairs;
+  pairs.insert(std::make_pair(2, "b"));
+  REQUIRE(res.pairVals == pairs);
+}
+
+TEST_CASE("RelationTableManager known arg2 with arg1 predicate") {
+  RelationTableManager<int, string> tableManager(
+      make_shared<ContiguousTable<string>>(),
+      make_shared<HashKeyTable<string, int>>());
+
+  tableManager.insert(1, "a");
+  tableManager.insert(2, "b");
+  tableManager.insert(4, "a");
+
+  unordered_set<int> validValues({2, 4});
+  Predicate<int> isValid = [validValues](int const &s) {
+    return validValues.find(s) != validValues.end();
+  };
+  auto res = tableManager.query(isValid, "a");
+
+  REQUIRE(res.firstArgVals == unordered_set<int>({4}));
+  REQUIRE(res.secondArgVals == unordered_set<string>({"a"}));
+
+  unordered_set<std::pair<int, string>> pairs;
+  pairs.insert(std::make_pair(4, "a"));
+  REQUIRE(res.pairVals == pairs);
+}
