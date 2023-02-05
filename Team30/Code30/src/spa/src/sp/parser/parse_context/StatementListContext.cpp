18f80369c18198967b9dea3a02ac65a4bbd207fb
Wei-Xinn <E0544325@u.nus.edu>
2023-02-05 23:58:47 +0800

    feat: add statement list context

---
+++ Team30/Code30/src/spa/src/sp/parser/parse_context/StatementListContext.cpp
@@ -0,0 +1,26 @@
+#include <memory>
+#include "StatementContext.h"
+#include "../../errors/SPError.h"
+#include "sp/common/ASTNode/StatementListNode.h"
+#include "StatementListContext.h"
+
+using std::shared_ptr;
+
+bool StatementListContext::validate(SourceParseState *state) {
+  return true;
+}
+
+shared_ptr<ASTNode> StatementListContext::generateSubtree(SourceParseState *state) {
+  shared_ptr<StatementListNode> node = shared_ptr<StatementListNode>(new StatementListNode());
+
+  expect(state, SIMPLE_TOKEN_BRACKET_CURLY_LEFT);
+  while (!state->getCurrToken()->isType(SIMPLE_TOKEN_BRACKET_CURLY_RIGHT)) {
+    shared_ptr<ASTNode> newNode = contextProvider->
+        getContext(STMT_CONTEXT)->generateSubtree(state);
+    node->addChild(newNode);
+  }
+  expect(state, SIMPLE_TOKEN_BRACKET_CURLY_RIGHT);
+  state->setCached(node);
+
+  return node;
+}
