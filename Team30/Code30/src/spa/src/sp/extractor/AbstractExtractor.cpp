defd52261ea4da521d4d8eb12665a572478eea2c
Abdulelah Faisal S Al Ghrairy <e0556733@u.nus.edu>
2023-02-06 23:51:52 +0800

    chore: fix linting

--- Team30/Code30/src/spa/src/sp/extractor/AbstractExtractor.cpp
+++ Team30/Code30/src/spa/src/sp/extractor/AbstractExtractor.cpp
@@ -1,12 +1,5 @@
 #include "AbstractExtractor.h"
 
-#include "sp/common/ASTNode/Statement/AssignNode.h"
-#include "sp/common/ASTNode/Statement/PrintNode.h"
-#include "sp/common/ASTNode/Statement/WhileNode.h"
-#include "sp/common/ASTNode/StatementListNode.h"
-#include "sp/common/ASTNode/statement/IfNode.h"
-#include "sp/common/ASTNode/statement/ReadNode.h"
-
 void AbstractExtractor::visit(IfNode node) {}
 
 void AbstractExtractor::visit(ReadNode node) {}
