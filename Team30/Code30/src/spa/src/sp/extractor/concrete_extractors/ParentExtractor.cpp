48e77d221cea871177b3f48dcc8c3cac9f641136
Abdulelah Faisal S Al Ghrairy <e0556733@u.nus.edu>
2023-02-07 01:18:20 +0800

    fix: fix else statement list being excluded from ParentExtractor

--- Team30/Code30/src/spa/src/sp/extractor/concrete_extractors/ParentExtractor.cpp
+++ Team30/Code30/src/spa/src/sp/extractor/concrete_extractors/ParentExtractor.cpp
@@ -1,8 +1,6 @@
 #include "ParentExtractor.h"
 
-ParentExtractor::ParentExtractor(PkbWriter* writer) {
-  pkbWriter = writer;
-}
+ParentExtractor::ParentExtractor(PkbWriter* writer) { pkbWriter = writer; }
 
 void ParentExtractor::visit(IfNode node) {
   vector<shared_ptr<ASTNode>> children = node.getChildren();
@@ -16,11 +14,19 @@ void ParentExtractor::visit(IfNode node) {
 
 void ParentExtractor::visit(WhileNode node) {
   vector<shared_ptr<ASTNode>> children = node.getChildren();
-  vector<shared_ptr<ASTNode>> stmtlst = children[1]->getChildren();
-  for (int i = 0; i < stmtlst.size(); i++) {
+  vector<shared_ptr<ASTNode>> ifStmtLst = children[1]->getChildren();
+  vector<shared_ptr<ASTNode>> elseStmtLst = children[2]->getChildren();
+  for (int i = 0; i < ifStmtLst.size(); i++) {
     ParentExtractor::addParentRelation(
         node.lineNumber,
-        std::dynamic_pointer_cast<StatementASTNode>(stmtlst[i])->lineNumber);
+        std::dynamic_pointer_cast<StatementASTNode>(ifStmtLst[i])->lineNumber);
+  }
+
+  for (int i = 0; i < elseStmtLst.size(); i++) {
+    ParentExtractor::addParentRelation(
+        node.lineNumber,
+        std::dynamic_pointer_cast<StatementASTNode>(elseStmtLst[i])
+            ->lineNumber);
   }
 }
 
