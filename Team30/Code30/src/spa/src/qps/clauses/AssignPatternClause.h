02ce613e4dc1aac19931669b91a5b999761789a7
sharpstorm <chimchar17@gmail.com>
2023-02-09 15:33:11 +0800

    feat: add pattern clause definition

---
+++ Team30/Code30/src/spa/src/qps/clauses/AssignPatternClause.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include "PatternClause.h"
+#include "ClauseArgument.h"
+
+using std::string;
+
+class AssignPatternClause: public PatternClause {
+ private:
+  PQLQueryVariable assignSynonym;
+  ClauseArgument leftArgument;
+  string patternPhrase;
+  bool allowPartial;
+
+ public:
+  AssignPatternClause(PQLQueryVariable assignSynonym,
+                 ClauseArgument leftSynonym,
+                 string patternPhrase,
+                 bool allowPartial);
+  PQLQueryResult* evaluateOn(shared_ptr<PkbQueryHandler> pkbQueryHandler);
+  bool validateArgTypes(VariableTable *variables);
+  bool usesSynonym(string varName);
+};
