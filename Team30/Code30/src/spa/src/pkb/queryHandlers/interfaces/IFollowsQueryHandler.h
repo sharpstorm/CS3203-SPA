120ddcd7df12dc67ea449d889d03a71401a6716c
sharpstorm <chimchar17@gmail.com>
2023-02-09 23:17:19 +0800

    refactor: change visibility of PKB interfaces

--- Team30/Code30/src/spa/src/pkb/queryHandlers/interfaces/IFollowsQueryHandler.h
+++ Team30/Code30/src/spa/src/pkb/queryHandlers/interfaces/IFollowsQueryHandler.h
@@ -3,6 +3,7 @@
 #include "../../../common/Types.h"
 
 class IFollowsQueryHandler {
+ public:
   virtual QueryResult<int, int> queryFollows(StmtRef s1, StmtRef s2) const = 0;
   virtual QueryResult<int, int> queryFollowsStar(StmtRef s1, StmtRef s2)
       const = 0;
--- Team30/Code30/src/spa/src/pkb/queryHandlers/interfaces/IParentQueryHandler.h
+++ Team30/Code30/src/spa/src/pkb/queryHandlers/interfaces/IParentQueryHandler.h
@@ -3,6 +3,7 @@
 #include "../../../common/Types.h"
 
 class IParentQueryHandler {
+ public:
   virtual QueryResult<int, int> queryParent(StmtRef s1, StmtRef s2) const = 0;
   virtual QueryResult<int, int> queryParentStar(StmtRef s1, StmtRef s2)
       const = 0;
