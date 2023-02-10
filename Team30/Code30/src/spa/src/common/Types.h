fb5a858854eae73b413c3fa5e7da3b58bf424863
lingshanng <nglingshan456@gmail.com>
2023-02-10 09:27:15 +0800

    feat: modify common types

--- Team30/Code30/src/spa/src/common/Types.h
+++ Team30/Code30/src/spa/src/common/Types.h
@@ -6,31 +6,38 @@
 
 using std::pair;
 using std::unordered_set;
+using std::string;
 
-enum class EntityType { Statement, Variable, Constant, None };
+enum class EntityType { Procedure, Variable, Constant, None };
 
 enum class StmtType { Read, Print, Assign, Call, While, If, None };
 
 struct StmtRef {
   StmtType type;
   int lineNum;
+  bool isKnown() {
+    return lineNum != 0;
+  }
 };
 
 struct EntityRef {
   EntityType type;
-  std::string name;
+  string name;
+  bool isKnown() {
+    return name.empty();
+  }
 };
 
-template <class T1, class T2>
+template<class T1, class T2>
 struct std::hash<pair<T1, T2>> {
-  std::size_t operator()(const pair<T1, T2>& pair) const {
+  std::size_t operator()(const pair<T1, T2> &pair) const {
     return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
   }
 };
-template <typename K, typename V>
+template<typename K, typename V>
 using pair_set = unordered_set<pair<K, V>>;
 
-template <typename T, typename U>
+template<typename T, typename U>
 struct QueryResult {
   unordered_set<T> firstArgVals;
   unordered_set<U> secondArgVals;
