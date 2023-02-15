4844e0af260694cd18c7ad3d362c843053d934ab
sharpstorm <chimchar17@gmail.com>
2023-02-16 00:58:25 +0800

    chore: fix linting

--- Team30/Code30/src/spa/src/qps/errors/QPSLexerError.h
+++ Team30/Code30/src/spa/src/qps/errors/QPSLexerError.h
@@ -2,8 +2,6 @@
 
 #include "QPSError.h"
 
-using std::string;
-
 const char QPS_LEXER_ERR_INTEGER_ZERO[] = "Integer token starts with zero";
 const char QPS_LEXER_ERR_STRING_DIGIT[] = "String token starts with digit";
 const char QPS_LEXER_ERR_INVALID_CHAR[] = "Detected invalid character";
