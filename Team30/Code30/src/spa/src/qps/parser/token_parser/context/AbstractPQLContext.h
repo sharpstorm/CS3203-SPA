98b72e3e90b2c08a2011ad3e3d0fa4c4de42eac6
sharpstorm <chimchar17@gmail.com>
2023-02-15 16:51:44 +0800

    refactor: tidy abstract PQL context

--- Team30/Code30/src/spa/src/qps/parser/token_parser/context/AbstractPQLContext.h
+++ Team30/Code30/src/spa/src/qps/parser/token_parser/context/AbstractPQLContext.h
@@ -7,14 +7,4 @@
 class AbstractPQLContext: public IPQLContext {
  public:
   virtual void parse(QueryTokenParseState* parserState) = 0;
-
- protected:
-  ClauseArgument extractStatementRef(QueryTokenParseState* state);
-  ClauseArgument extractEntityRef(QueryTokenParseState* state);
-  ClauseArgument extractAnyRef(QueryTokenParseState* state);
-
- private:
-  ClauseArgument extractCommonRef(QueryTokenParseState* state);
-  ClauseArgument extractEntity(QueryTokenParseState* state);
-  ClauseArgument extractStatement(QueryTokenParseState* state);
 };
