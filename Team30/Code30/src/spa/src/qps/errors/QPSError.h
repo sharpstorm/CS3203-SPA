f6c29833a47f8b8ff0953404b36515d7b7338967
sharpstorm <chimchar17@gmail.com>
2023-02-16 01:15:58 +0800

    fix: fix std exception compatability

--- Team30/Code30/src/spa/src/qps/errors/QPSError.h
+++ Team30/Code30/src/spa/src/qps/errors/QPSError.h
@@ -1,6 +1,14 @@
 #pragma once
 
 class QPSError : public std::exception {
+ private:
+  const char* errMsg;
+
  protected:
-  explicit QPSError(const char* message): exception(message) {}
+  explicit QPSError(const char* message): errMsg(message) {}
+
+ public:
+  const char* what() const noexcept override {
+    return errMsg;
+  }
 };
