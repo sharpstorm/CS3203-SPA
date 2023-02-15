11ceb14aa7d2c850ff45c89a22a19e78d7d027e0
sharpstorm <chimchar17@gmail.com>
2023-02-16 02:16:22 +0800

    chore: add new QPS error message

--- Team30/Code30/src/spa/src/qps/errors/QPSParserSemanticError.h
+++ Team30/Code30/src/spa/src/qps/errors/QPSParserSemanticError.h
@@ -6,6 +6,8 @@ const char QPS_PARSER_ERR_UNKNOWN_SYNONYM[] = "Unknown synonym encountered";
 const char QPS_PARSER_ERR_PATTERN_TYPE[] = "Invalid synonym type in pattern";
 const char QPS_PARSER_ERR_SYNONYM_TYPE[] = "Invalid synonym type in clause";
 const char QPS_PARSER_ERR_DUPLICATE_SYN[] = "Found duplicate synonym name";
+const char QPS_PARSER_ERR_UNKNOWN_SELECT[] =
+    "Select clause references unknown synonym";
 
 class QPSParserSemanticError: public QPSError {
  public:
