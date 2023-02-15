09426b3dd9e9b2191782467c6960a4a47e4f9df5
sharpstorm <chimchar17@gmail.com>
2023-02-15 17:08:26 +0800

    refactor: move QPS parser errors

--- Team30/Code30/src/spa/src/qps/parser/token_parser/PQLParserErrors.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-const char QPS_PARSER_ERR_EOS[] = "Unexpected end of token stream";
-const char QPS_PARSER_ERR_UNEXPECTED[] =
-    "Unexpected token encountered during parsing";
-const char QPS_PARSER_ERR_UNKNOWN_TOKEN[] = "Unknown synonym encountered";
-const char QPS_PARSER_ERR_PATTERN_TYPE[] = "Invalid synonym type in pattern";
-const char QPS_PARSER_ERR_TOKEN_SEQUENCE[] = "Unexpected sequence of clauses";
