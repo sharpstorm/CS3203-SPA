153fa3c0ae6204c26708849c23fced06f94ca3bc
Abdulelah Faisal S Al Ghrairy <e0556733@u.nus.edu>
2023-02-17 14:15:29 +0800

    test: modify FollowsExtractorTest

--- Team30/Code30/src/unit_testing/src/sp/extractors/FollowsExtractorTest.cpp
+++ Team30/Code30/src/unit_testing/src/sp/extractors/FollowsExtractorTest.cpp
@@ -2,6 +2,8 @@
 
 #include <memory>
 #include <unordered_set>
+#include <utility>
+#include <vector>
 
 #include "../../../../spa/src/pkb/storage/TransitiveRelationTableManager.h"
 #include "../../../../spa/src/pkb/storage/tables/ContiguousSetTable.h"
@@ -9,127 +11,121 @@
 #include "catch.hpp"
 #include "sp/extractor/concrete_extractors/FollowsExtractor.h"
 
-using std::make_shared;
-using std::unordered_set;
-
 class PkbWriterStubForFollows : public PkbWriter {
  public:
-  TransitiveRelationTableManager<int>* storage;
+  vector<vector<int>> relations{};
 
-  PkbWriterStubForFollows(PKB* pkb, TransitiveRelationTableManager<int>* store)
-      : PkbWriter(pkb) {
-    storage = store;
+  PkbWriterStubForFollows(PKB* pkb) : PkbWriter(pkb) {
+    for (int i = 0; i <= 10; i++) {
+      relations.push_back(new vector<int>{});
+    }
   }
 
-  void addFollows(int arg1, int arg2) final { storage->insert(arg1, arg2); }
-};
+  void addFollows(int arg1, int arg2) final {
+    relations.at(arg1).push_back(arg2);
+  };
 
-shared_ptr<StatementListNode> simplyLst() {
-  shared_ptr<StatementListNode> simple =
-      shared_ptr<StatementListNode>(new StatementListNode());
+  shared_ptr<StatementListNode> simplyLst() {
+    shared_ptr<StatementListNode> simple =
+        shared_ptr<StatementListNode>(new StatementListNode());
 
-  shared_ptr<StatementASTNode> first =
-      shared_ptr<StatementASTNode>(new PrintNode());
-  shared_ptr<StatementASTNode> second =
-      shared_ptr<StatementASTNode>(new PrintNode());
-  shared_ptr<StatementASTNode> third =
-      shared_ptr<StatementASTNode>(new PrintNode());
+    shared_ptr<StatementASTNode> first =
+        shared_ptr<StatementASTNode>(new PrintNode());
+    shared_ptr<StatementASTNode> second =
+        shared_ptr<StatementASTNode>(new PrintNode());
+    shared_ptr<StatementASTNode> third =
+        shared_ptr<StatementASTNode>(new PrintNode());
 
-  first->lineNumber = 1;
-  second->lineNumber = 2;
-  third->lineNumber = 3;
+    first->lineNumber = 1;
+    second->lineNumber = 2;
+    third->lineNumber = 3;
 
-  simple->addChild(first);
-  simple->addChild(second);
-  simple->addChild(third);
-  return simple;
-}
+    simple->addChild(first);
+    simple->addChild(second);
+    simple->addChild(third);
+    return simple;
+  }
 
-shared_ptr<StatementListNode> stmListWithWhile() {
-  shared_ptr<StatementListNode> simple =
-      shared_ptr<StatementListNode>(new StatementListNode());
+  shared_ptr<StatementListNode> stmListWithWhile() {
+    shared_ptr<StatementListNode> simple =
+        shared_ptr<StatementListNode>(new StatementListNode());
 
-  shared_ptr<StatementASTNode> first =
-      shared_ptr<StatementASTNode>(new PrintNode());
-  shared_ptr<StatementASTNode> second =
-      shared_ptr<StatementASTNode>(new PrintNode());
-  shared_ptr<StatementASTNode> third =
-      shared_ptr<StatementASTNode>(new PrintNode());
+    shared_ptr<StatementASTNode> first =
+        shared_ptr<StatementASTNode>(new PrintNode());
+    shared_ptr<StatementASTNode> second =
+        shared_ptr<StatementASTNode>(new PrintNode());
+    shared_ptr<StatementASTNode> third =
+        shared_ptr<StatementASTNode>(new PrintNode());
 
-  shared_ptr<WhileNode> simpleWhile = shared_ptr<WhileNode>(new WhileNode());
+    shared_ptr<WhileNode> simpleWhile = shared_ptr<WhileNode>(new WhileNode());
 
-  shared_ptr<StatementListNode> stmtLst =
-      shared_ptr<StatementListNode>(new StatementListNode());
+    shared_ptr<StatementListNode> stmtLst =
+        shared_ptr<StatementListNode>(new StatementListNode());
 
-  shared_ptr<StatementASTNode> firstWChild =
-      shared_ptr<StatementASTNode>(new PrintNode());
-  shared_ptr<StatementASTNode> secondWChild =
-      shared_ptr<StatementASTNode>(new PrintNode());
-  shared_ptr<StatementASTNode> thirdWChild =
-      shared_ptr<StatementASTNode>(new AssignNode());
+    shared_ptr<StatementASTNode> firstWChild =
+        shared_ptr<StatementASTNode>(new PrintNode());
+    shared_ptr<StatementASTNode> secondWChild =
+        shared_ptr<StatementASTNode>(new PrintNode());
+    shared_ptr<StatementASTNode> thirdWChild =
+        shared_ptr<StatementASTNode>(new AssignNode());
 
-  shared_ptr<ConditionalExpressionASTNode> condition =
-      shared_ptr<ConditionalExpressionASTNode>();
+    shared_ptr<ConditionalExpressionASTNode> condition =
+        shared_ptr<ConditionalExpressionASTNode>();
 
-  first->lineNumber = 1;
-  second->lineNumber = 2;
-  simpleWhile->lineNumber = 3;
-  firstWChild->lineNumber = 4;
-  secondWChild->lineNumber = 5;
-  thirdWChild->lineNumber = 6;
-  third->lineNumber = 7;
+    first->lineNumber = 1;
+    second->lineNumber = 2;
+    simpleWhile->lineNumber = 3;
+    firstWChild->lineNumber = 4;
+    secondWChild->lineNumber = 5;
+    thirdWChild->lineNumber = 6;
+    third->lineNumber = 7;
 
-  simple->addChild(first);
-  simple->addChild(second);
-  simple->addChild(third);
-  stmtLst->addChild(first);
-  stmtLst->addChild(second);
-  stmtLst->addChild(third);
+    simple->addChild(first);
+    simple->addChild(second);
+    simple->addChild(third);
+    stmtLst->addChild(first);
+    stmtLst->addChild(second);
+    stmtLst->addChild(third);
 
-  simpleWhile->setChild(0, condition);
-  simpleWhile->setChild(1, stmtLst);
+    simpleWhile->setChild(0, condition);
+    simpleWhile->setChild(1, stmtLst);
 
-  simple->addChild(simpleWhile);
-  return simple;
-}
+    simple->addChild(simpleWhile);
+    return simple;
+  }
 
-TEST_CASE("FollowsExtractor simpleStmtLst") {
-  shared_ptr<StatementListNode> simple = simplyLst();
+  TEST_CASE("FollowsExtractor simpleStmtLst") {
+    shared_ptr<StatementListNode> simple = simplyLst();
 
-  auto table = make_shared<ContiguousSetTable<int>>();
-  auto reverseTable = make_shared<ContiguousSetTable<int>>();
-  auto store = new TransitiveRelationTableManager<int>(table, reverseTable);
-  PKB* pkb = new PKB();
+    PKB* pkb = new PKB();
 
-  PkbWriterStubForFollows writer = PkbWriterStubForFollows(pkb, store);
+    PkbWriterStubForFollows writer = PkbWriterStubForFollows(pkb, store);
 
-  FollowsExtractor* extractor = new FollowsExtractor(&writer);
+    FollowsExtractor* extractor = new FollowsExtractor(&writer);
 
-  extractor->visit(*simple);
+    extractor->visit(*simple);
 
-  REQUIRE(table->get(1) == unordered_set<int>({2}));
-  REQUIRE(reverseTable->get(2) == unordered_set<int>({1}));
-  REQUIRE(table->get(2) == unordered_set<int>({3}));
-  REQUIRE(reverseTable->get(3) == unordered_set<int>({2}));
-}
+    REQUIRE(writer.relations.at(1) == {2});
+    REQUIRE(writer.relations.at(2) == {3});
+  }
 
-TEST_CASE("FollowsExtractor Statement with While loop inbetween") {
-  shared_ptr<StatementListNode> simple = simplyLst();
+  // TEST_CASE("FollowsExtractor Statement with While loop inbetween") {
+  //  shared_ptr<StatementListNode> simple = simplyLst();
 
-  auto table = make_shared<ContiguousSetTable<int>>();
-  auto reverseTable = make_shared<ContiguousSetTable<int>>();
-  auto store = new TransitiveRelationTableManager<int>(table, reverseTable);
-  PKB* pkb = new PKB();
+  //  auto table = make_shared<ContiguousSetTable<int>>();
+  //  auto reverseTable = make_shared<ContiguousSetTable<int>>();
+  //  auto store = new TransitiveRelationTableManager<int>(table, reverseTable);
+  //  PKB* pkb = new PKB();
 
-  PkbWriterStubForFollows writer = PkbWriterStubForFollows(pkb, store);
+  //  PkbWriterStubForFollows writer = PkbWriterStubForFollows(pkb, store);
 
-  FollowsExtractor* extractor = new FollowsExtractor(&writer);
+  //  FollowsExtractor* extractor = new FollowsExtractor(&writer);
 
-  extractor->visit(*simple);
+  //  extractor->visit(*simple);
 
-  REQUIRE(table->get(1) == unordered_set<int>({2}));
-  REQUIRE(table->get(2) == unordered_set<int>({3}));
-  REQUIRE(table->get(3) !=
-          unordered_set<int>(
-              {4}));  // statement 4 should not follow from statement 3
-}
+  //  REQUIRE(table->get(1) == unordered_set<int>({2}));
+  //  REQUIRE(table->get(2) == unordered_set<int>({3}));
+  //  REQUIRE(table->get(3) !=
+  //          unordered_set<int>(
+  //              {4}));  // statement 4 should not follow from statement 3
+  //}
