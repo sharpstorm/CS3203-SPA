2857690d661bf83f4f1c0a6727bc41bf3601d727
Abdulelah Faisal S Al Ghrairy <e0556733@u.nus.edu>
2023-02-18 03:36:26 +0800

    test: refactor test cases

--- Team30/Code30/src/unit_testing/src/sp/SourceTestCasesParseStressTest.cpp
+++ Team30/Code30/src/unit_testing/src/sp/SourceTestCasesParseStressTest.cpp
@@ -19,7 +19,7 @@
 #include "../../../spa/src/sp/extractor/concrete_extractors/UsesExtractor.h"
 #include "SourceTestCases.cpp"
 
-AST executeExtractors(string input) {
+void executeExtractors(string input) {
   TreeWalker treeWalker;
   PKB* pkb = new PKB();
   PkbWriter* pkbWriter = new PkbWriter(pkb);
@@ -45,220 +45,141 @@ AST executeExtractors(string input) {
 
   AST ast = parser.parseSource(input);
   treeWalker.walkAST(ast, extractors);
-
-  return ast;
 }
 
 TEST_CASE("Simple programs for grammar testing") {
   SourceTestCases testcase = SourceTestCases();
 
-  AST ast = executeExtractors(testcase.EMPTY);
+  REQUIRE_NOTHROW(executeExtractors(testcase.EMPTY));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.SIMPLE_PROGRAM));
 
-  ast = executeExtractors(testcase.SIMPLE_PROGRAM);
+  REQUIRE_NOTHROW(executeExtractors(testcase.ONE_LINE));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.SAME_NAMES));
 
-  ast = executeExtractors(testcase.ONE_LINE);
+  REQUIRE_NOTHROW(executeExtractors(testcase.CASE_SENSITIVE));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.LONG_NAMES));
 
-  ast = executeExtractors(testcase.SAME_NAMES);
+  REQUIRE_NOTHROW(executeExtractors(testcase.WHITESPACE));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.TEST_ASSIGN));
 
-  ast = executeExtractors(testcase.CASE_SENSITIVE);
+  REQUIRE_NOTHROW(executeExtractors(testcase.ASSIGN_TO_SELF));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.TEST_BASIC_PROCEDURE));
 
-  ast = executeExtractors(testcase.LONG_NAMES);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.WHITESPACE);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.TEST_ASSIGN);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.ASSIGN_TO_SELF);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.TEST_BASIC_PROCEDURE);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.TEST_OPERATORS);
-
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.TEST_OPERATORS));
 
   /*
 
-  ast = executeExtractors(testcase.CALL_PROCEDURES);
+   REQUIRE_NOTHROW(executeExtractors(testcase.CALL_PROCEDURES));
 
-  REQUIRE(ast.getRoot() );*/
+  REQUIRE(ast.getRoot() ));*/
 
   /*
 
-    ast = executeExtractors(testcase.CALL_CASE_SENSITIVE);
+     REQUIRE_NOTHROW(executeExtractors(testcase.CALL_CASE_SENSITIVE));
 
-    REQUIRE(ast.getRoot() );*/
+    REQUIRE(ast.getRoot() ));*/
 }
 
 TEST_CASE("Programs for testing simple if/else cases") {
   SourceTestCases testcase = SourceTestCases();
 
-  AST ast = executeExtractors(testcase.SIMPLE_IF);
+  REQUIRE_NOTHROW(executeExtractors(testcase.SIMPLE_IF));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.ONE_LINE_IF_STMT));
 
-  ast = executeExtractors(testcase.ONE_LINE_IF_STMT);
+  REQUIRE_NOTHROW(executeExtractors(testcase.SIMPLE_IF_ELSE));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.EMPTY_IF));
 
-  ast = executeExtractors(testcase.SIMPLE_IF_ELSE);
+  REQUIRE_NOTHROW(executeExtractors(testcase.TEST_CONDITIONAL));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.TEST_RELATIONAL));
 
-  ast = executeExtractors(testcase.EMPTY_IF);
+  REQUIRE_NOTHROW(executeExtractors(testcase.CHAINED_IF));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.MULTIPLE_IF_ELSE));
 
-  ast = executeExtractors(testcase.TEST_CONDITIONAL);
+  REQUIRE_NOTHROW(executeExtractors(testcase.CHAINED_IF_ELSE));
 
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.TEST_RELATIONAL);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.CHAINED_IF);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.MULTIPLE_IF_ELSE);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.CHAINED_IF_ELSE);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.MULTI_CHAINED_IF_ELSE);
-
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.MULTI_CHAINED_IF_ELSE));
 
   /*
 
-  ast = executeExtractors(testcase.CALL_IN_IF);
+   REQUIRE_NOTHROW(executeExtractors(testcase.CALL_IN_IF));
 
-  REQUIRE(ast.getRoot() );*/
+  REQUIRE(ast.getRoot() ));*/
 }
 
 TEST_CASE("Programs testing simple while cases") {
   SourceTestCases testcase = SourceTestCases();
 
-  AST ast = executeExtractors(testcase.SIMPLE_WHILE);
+  REQUIRE_NOTHROW(executeExtractors(testcase.SIMPLE_WHILE));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.WHILE_WOUT_WHITESPACE));
 
-  ast = executeExtractors(testcase.WHILE_WOUT_WHITESPACE);
+  REQUIRE_NOTHROW(executeExtractors(testcase.MULTIPLE_WHILE));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.EMPTY_WHILE));
 
-  ast = executeExtractors(testcase.MULTIPLE_WHILE);
+  REQUIRE_NOTHROW(executeExtractors(testcase.WHILE_INBETWEEN_STATEMENTS));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.CHAINED_WHILE));
 
-  ast = executeExtractors(testcase.EMPTY_WHILE);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.WHILE_INBETWEEN_STATEMENTS);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.CHAINED_WHILE);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.MULTI_CHAINED_WHILE);
-
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.MULTI_CHAINED_WHILE));
 
   /*
 
-  ast = executeExtractors(testcase.CALL_IN_WHILE);
+   REQUIRE_NOTHROW(executeExtractors(testcase.CALL_IN_WHILE));
 
-  REQUIRE(ast.getRoot() );*/
+  REQUIRE(ast.getRoot() ));*/
 
-  ast = executeExtractors(testcase.IF_IN_WHILE);
+  REQUIRE_NOTHROW(executeExtractors(testcase.IF_IN_WHILE));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.WHILE_IN_IF));
 
-  ast = executeExtractors(testcase.WHILE_IN_IF);
+  REQUIRE_NOTHROW(executeExtractors(testcase.WHILE_IN_IF_ELSE));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.MULTIPLE_WHILE_IN_IF_ELSE));
 
-  ast = executeExtractors(testcase.WHILE_IN_IF_ELSE);
+  REQUIRE_NOTHROW(executeExtractors(testcase.WHILE_CHAIN_IN_IF));
 
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.WHILE_CHAIN_IN_ELSE));
 
-  ast = executeExtractors(testcase.MULTIPLE_WHILE_IN_IF_ELSE);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.WHILE_CHAIN_IN_IF);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.WHILE_CHAIN_IN_ELSE);
-
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.WHILE_CHAIN_IN_IF_ELSE);
-
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.WHILE_CHAIN_IN_IF_ELSE));
 
   /*
 
-  ast = executeExtractors(testcase.IF_ELSE_SINGLE_CALL_IN_WHILE);
+   REQUIRE_NOTHROW(executeExtractors(testcase.IF_ELSE_SINGLE_CALL_IN_WHILE));
 
-  REQUIRE(ast.getRoot() );*/
+  REQUIRE(ast.getRoot() ));*/
 
   /*
 
-  ast = executeExtractors(testcase.MULTIPLE_CALLS_IN_WHILE);
+   REQUIRE_NOTHROW(executeExtractors(testcase.MULTIPLE_CALLS_IN_WHILE));
 
-  REQUIRE(ast.getRoot() );*/
+  REQUIRE(ast.getRoot() ));*/
 
-  ast = executeExtractors(testcase.CHAINED_WHILE_WITH_IF_IN_BODY);
+  REQUIRE_NOTHROW(executeExtractors(testcase.CHAINED_WHILE_WITH_IF_IN_BODY));
 
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.MULTIPLE_IF_ELSE_WITH_WHILE_CHAIN);
-
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(
+      executeExtractors(testcase.MULTIPLE_IF_ELSE_WITH_WHILE_CHAIN));
 }
 
 TEST_CASE("Complex Programs") {
   SourceTestCases testcase = SourceTestCases();
 
-  AST ast = executeExtractors(testcase.COMPLEX_PROGRAM_ONE);
+  REQUIRE_NOTHROW(executeExtractors(testcase.COMPLEX_PROGRAM_ONE));
 
-  REQUIRE(ast.getRoot());
-
-  ast = executeExtractors(testcase.COMPLEX_PROGRAM_TWO);
-
-  REQUIRE(ast.getRoot());
+  REQUIRE_NOTHROW(executeExtractors(testcase.COMPLEX_PROGRAM_TWO));
 
   /*
 
-  ast = executeExtractors(testcase.COMPLEX_PROGRAM_THREE);
+   REQUIRE_NOTHROW(executeExtractors(testcase.COMPLEX_PROGRAM_THREE));
 
-  REQUIRE(ast.getRoot() );*/
+  REQUIRE(ast.getRoot() ));*/
 }
