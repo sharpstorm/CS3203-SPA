5c628e47a359be3500d0e2ade12dd5a349919b22
Abdulelah Faisal S Al Ghrairy <e0556733@u.nus.edu>
2023-02-17 08:18:52 +0800

    test: modify the ModifyExtractor test cases

--- Team30/Code30/src/unit_testing/src/sp/extractors/ModifiesExtractorTest.cpp
+++ Team30/Code30/src/unit_testing/src/sp/extractors/ModifiesExtractorTest.cpp
@@ -5,64 +5,60 @@
 #include "../../../../spa/src/common/ASTNode/VariableASTNode.h"
 #include "../../../../spa/src/common/ASTNode/statement/AssignNode.h"
 #include "../../../../spa/src/common/ASTNode/statement/PrintNode.h"
+#include "../../../../spa/src/common/ASTNode/statement/ReadNode.h"
 #include "../../../../spa/src/pkb/storage/PKB.h"
 #include "../../../../spa/src/pkb/writers/PkbWriter.h"
 #include "../../../../spa/src/sp/extractor/concrete_extractors/ModifiesExtractor.h"
+
 class PkbWriterStubforModifies : public PkbWriter {
  public:
   int num;
-  string value;
+  std::string value;
 
   PkbWriterStubforModifies(PKB* pkb) : PkbWriter(pkb) {}
 
-  void addModifies(int stmtNum, string variable) final {
+  void addModifies(int stmtNum, std::string variable) final {
     num = stmtNum;
     value = variable;
   }
 
-  int verifyEntry(int stmtNum, string variable) {
+  int verifyEntry(int stmtNum, std::string variable) {
     return (num == stmtNum) && (variable == value);
   }
 };
 
-//TEST_CASE("ModifiesExtractor AssignNode") {
-//  AssignNode* node = new AssignNode();
-//
-//  shared_ptr<StatementASTNode> child =
-//      shared_ptr<StatementASTNode>(new PrintNode());
-//
-//  node->lineNumber = 3;
-//  node->addChild(child);
-//
-//  string value = std::dynamic_pointer_cast<VariableASTNode>(node->getChildren()[0])->getValue();
-//
-//  PKB* pkb = new PKB();
-//  PkbWriterStubforModifies* writer = new PkbWriterStubforModifies(pkb);
-//  ModifiesExtractor* extractor = new ModifiesExtractor(writer);
-//
-//  extractor->visit(*node);
-//
-//  REQUIRE(writer->verifyEntry(node->lineNumber, value));
-//}
-//
-//TEST_CASE("ModifiesExtractor ReadNode") {
-//  ReadNode* node = new ReadNode();
-//
-//  shared_ptr<StatementASTNode> child =
-//      shared_ptr<StatementASTNode>(new PrintNode());
-//
-//  node->lineNumber = 4;
-//  node->addChild(child);
-//
-//  string value =
-//      std::dynamic_pointer_cast<VariableASTNode>(node->getChildren()[0])
-//          ->getValue();
-//
-//  PKB* pkb = new PKB();
-//  PkbWriterStubforModifies* writer = new PkbWriterStubforModifies(pkb);
-//  ModifiesExtractor* extractor = new ModifiesExtractor(writer);
-//
-//  extractor->visit(*node);
-//
-//  REQUIRE(writer->verifyEntry(node->lineNumber, value));
-//}
+TEST_CASE("ModifiesExtractor AssignNode") {
+  shared_ptr<AssignNode> node = shared_ptr<AssignNode>(new AssignNode());
+
+  shared_ptr<VariableASTNode> child =
+      shared_ptr<VariableASTNode>(new VariableASTNode("TEST"));
+
+  node->lineNumber = 2;
+  node->addChild(child);
+
+  PKB* pkb = new PKB();
+  PkbWriterStubforModifies* writer = new PkbWriterStubforModifies(pkb);
+  ModifiesExtractor* extractor = new ModifiesExtractor(writer);
+
+  extractor->visit(*node);
+
+  REQUIRE(writer->verifyEntry(node->lineNumber, "TEST"));
+}
+
+TEST_CASE("ModifiesExtractor ReadNode") {
+  shared_ptr<ReadNode> node = shared_ptr<ReadNode>(new ReadNode());
+
+  shared_ptr<VariableASTNode> child =
+      shared_ptr<VariableASTNode>(new VariableASTNode("TEST"));
+
+  node->lineNumber = 2;
+  node->addChild(child);
+
+  PKB* pkb = new PKB();
+  PkbWriterStubforModifies* writer = new PkbWriterStubforModifies(pkb);
+  ModifiesExtractor* extractor = new ModifiesExtractor(writer);
+
+  extractor->visit(*node);
+
+  REQUIRE(writer->verifyEntry(node->lineNumber, "TEST"));
+}
