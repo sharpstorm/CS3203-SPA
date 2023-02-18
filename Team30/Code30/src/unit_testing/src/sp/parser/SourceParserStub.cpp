20b583247efd4d72e3d483dabcc87b92dbec85c0
Abdulelah Faisal S Al Ghrairy <e0556733@u.nus.edu>
2023-02-19 02:12:46 +0800

    test: delete unnecessary stub class

--- Team30/Code30/src/unit_testing/src/sp/parser/SourceParserStub.cpp
+++ Team30/Code30/src/unit_testing/src/sp/parser/SourceParserStub.cpp
@@ -3,16 +3,12 @@
 
 #include "../../../../spa/src/sp/SourceParser.h"
 #include "../../../../spa/src/sp/parser/SourceParseState.h"
-#include "../../../../unit_testing/src/sp/parser/SourceTokenParserStub.cpp"
 #include "sp/lexer/SourceLexer.h"
-#include "sp/parser/SourceTokenParser.h"
 
 class SourceParserStub : public SourceParser {
  public:
-  SourceParseState parseSource_AndGetState(string input) {
-    SourceLexer sourceLexer;
-    SourceTokenParserStub stub;
-    vector<SourceToken> tokens = sourceLexer.tokenize(&input);
-    return stub.parseAndGetState(&tokens);
+  SourceParseState parseTokens_AndGetState(vector<SourceToken> tokens) {
+    SourceParseState state(&tokens);
+    return state;
   }
 };
--- Team30/Code30/src/unit_testing/src/sp/parser/parse_context/math_context/ConditionalExpressionContextTest.cpp
+++ Team30/Code30/src/unit_testing/src/sp/parser/parse_context/math_context/ConditionalExpressionContextTest.cpp
@@ -3,19 +3,31 @@
 
 #include "../../../../../../../lib/catch.hpp"
 #include "../../../../../../spa/src/common/ASTNode/BinaryASTNode.h"
+#include "../../../../../../spa/src/sp/common/SourceToken.h"
 #include "../../../../../../spa/src/sp/parser/GrammarContextProvider.h"
 #include "../../../../../../spa/src/sp/parser/SourceParseState.h"
 #include "../../../../../../spa/src/sp/parser/parse_context/math_context/ConditionalExpressionContext.h"
 #include "../../SourceParserStub.cpp"
 
-SourceParseState executeParse(string input) {
+SourceParseState executeParse(vector<SourceToken> tokens) {
   SourceParserStub parser;
-  return parser.parseSource_AndGetState(input);
+  return parser.parseTokens_AndGetState(tokens);
+}
+
+vector<SourceToken> notConditionInput() {  //!(x && 1)
+  vector<SourceToken> tokens{SourceToken(SIMPLE_TOKEN_NOT, ""),
+                             SourceToken(SIMPLE_TOKEN_BRACKET_ROUND_LEFT, ""),
+                             SourceToken(SIMPLE_TOKEN_VARIABLE, "x"),
+                             SourceToken(SIMPLE_TOKEN_AND, ""),
+                             SourceToken(SIMPLE_TOKEN_VARIABLE, "y"),
+                             SourceToken(SIMPLE_TOKEN_BRACKET_ROUND_RIGHT, "")};
+
+  return tokens;
 }
 
 TEST_CASE("GenerateSubTree: ProcessNotCondition") {
-  string input = "!(num1 || num2)";
-  SourceParseState state = executeParse(input);
+  vector<SourceToken> tokens = notConditionInput();
+  SourceParseState state = executeParse(tokens);
   GrammarContextProvider gcp;
   ConditionalExpressionContext context(&gcp);
   shared_ptr<ASTNode> node = context.generateSubtree(&state);
