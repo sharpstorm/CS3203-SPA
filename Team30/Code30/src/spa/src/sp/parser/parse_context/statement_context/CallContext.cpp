2f37effaed8c23fb84d21faae206970acb3d7890
Wei-Xinn <E0544325@u.nus.edu>
2023-02-18 11:17:11 +0800

    fix: fix missing expect statement

--- Team30/Code30/src/spa/src/sp/parser/parse_context/statement_context/CallContext.cpp
+++ Team30/Code30/src/spa/src/sp/parser/parse_context/statement_context/CallContext.cpp
@@ -6,13 +6,16 @@
 using std::shared_ptr;
 
 shared_ptr<ASTNode> CallContext::generateSubtree(SourceParseState* state) {
-  // Procedure Node
+  // Call Node
   expect(state, SIMPLE_TOKEN_KEYWORD_CALL);
 
   // Name
   SourceToken* nameToken = expectVarchar(state);
   shared_ptr<CallNode> callNode =
       shared_ptr<CallNode>(new CallNode(nameToken->getValue()));
+
+  // Expect ';'
+  expect(state, SIMPLE_TOKEN_SEMICOLON);
 
   return callNode;
 }
