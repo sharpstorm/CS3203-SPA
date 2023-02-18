e75d7e0af17d09c855961628e1fa2e63a9c5d0be
ddx-510 <ddx0510@gmail.com>
2023-02-18 17:31:43 +0800

    test: add test for assigns

---
+++ Team30/Code30/src/unit_testing/src/pkb/AssignsTest.cpp
@@ -0,0 +1,24 @@
+#include <memory>
+#include <string>
+#include <unordered_set>
+
+#include "catch.hpp"
+#include "common/ASTNode/VariableASTNode.h"
+#include "common/Types.h"
+#include "pkb/queryHandlers/PkbQueryHandler.h"
+#include "pkb/storage/PKB.h"
+#include "pkb/writers/PkbWriter.h"
+
+using std::make_unique;
+using std::unordered_set;
+
+TEST_CASE("Assign write and read") {
+  auto pkb = make_unique<PKB>();
+  PkbWriter writer = PkbWriter(pkb.get());
+  PkbQueryHandler queryHandler = PkbQueryHandler(pkb.get());
+  shared_ptr<IASTNode> node1 = shared_ptr<IASTNode>(new VariableASTNode("a"));
+
+  writer.addAssigns(1, node1);
+  auto result = queryHandler.queryAssigns({StmtType::Assign, 1});
+  REQUIRE(result.pairVals == pair_set<int, shared_ptr<IASTNode>>({{1, node1}}));
+}
--- Team30/Code30/src/unit_testing/src/pkb/queryHandlers/AssignsQueryHandlerTest.cpp
+++ Team30/Code30/src/unit_testing/src/pkb/queryHandlers/AssignsQueryHandlerTest.cpp
@@ -1,10 +1,9 @@
 #include <memory>
 #include <unordered_set>
-#include <utility>
 
-#include "EntityMappingProviderStub.h"
 #include "catch.hpp"
 #include "common/ASTNode/VariableASTNode.h"
+#include "common/ASTNode/math/math_operand/PlusASTNode.h"
 #include "common/Types.h"
 #include "pkb/queryHandlers/AssignsQueryHandler.h"
 #include "pkb/storage/StructureMappingProvider.h"
@@ -20,20 +19,20 @@ struct assignTestInit {
   AssignsQueryHandler handler;
 
   assignTestInit()
-        :store(make_unique<AssignStorage>()),
+      : store(make_unique<AssignStorage>()),
         handler(AssignsQueryHandler(store.get())) {}
 };
 
-// Both args known
-TEST_CASE("AssignQueryHandler Assigns(stmtNum)") {
+// assign stmt
+TEST_CASE("AssignQueryHandler Assigns(stmtRef)") {
   auto test = assignTestInit();
+  // x = a
   shared_ptr<IASTNode> node1 = shared_ptr<IASTNode>(new VariableASTNode("a"));
-  shared_ptr<IASTNode> node2 = shared_ptr<IASTNode>(new VariableASTNode("b"));
   test.store->set(1, node1);
-  test.store->set(2, node2);
 
   auto result = test.handler.queryAssigns({StmtType::Assign, 1});
   REQUIRE(result.isEmpty == false);
   REQUIRE(result.firstArgVals == unordered_set<int>({1}));
   REQUIRE(*result.secondArgVals.begin() == node1);
+  REQUIRE(result.pairVals == pair_set<int, shared_ptr<IASTNode>>({{1, node1}}));
 }
