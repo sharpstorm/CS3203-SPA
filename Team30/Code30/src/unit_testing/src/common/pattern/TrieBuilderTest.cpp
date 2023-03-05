b41c78a2a7e8d862d0c6629b497adb79c10774eb
sharpstorm <chimchar17@gmail.com>
2023-03-05 14:27:12 +0800

    test: add trie tests

---
+++ Team30/Code30/src/unit_testing/src/common/pattern/TrieBuilderTest.cpp
@@ -0,0 +1,59 @@
+#include "catch.hpp"
+#include "TestASTProvider.h"
+#include "common/pattern/PatternConverter.h"
+
+void assertMatchFull(PatternTrie* trie, ExpressionSequence expected) {
+  REQUIRE(trie->isMatchPartial(&expected));
+  REQUIRE(trie->isMatchFull(&expected));
+}
+
+void assertMatchPartial(PatternTrie* trie, ExpressionSequence expected) {
+  REQUIRE(trie->isMatchPartial(&expected));
+  REQUIRE(!trie->isMatchFull(&expected));
+}
+
+void assertNotMatch(PatternTrie* trie, ExpressionSequence expected) {
+  REQUIRE(!trie->isMatchPartial(&expected));
+  REQUIRE(!trie->isMatchFull(&expected));
+}
+
+TEST_CASE("Trie Building - Simple Tree") {
+  TestASTProvider treeProvider;
+  auto trie = PatternConverter::convertASTToTrie(
+      treeProvider.leftHeavyTree->getRoot());
+  auto triePtr = trie.get();
+
+  // x * y + z;
+  assertMatchFull(triePtr, {"x", "y", "*", "z", "+"});
+
+  assertMatchPartial(triePtr, {"x", "y", "*"});
+  assertMatchPartial(triePtr, {"x"});
+  assertMatchPartial(triePtr, {"y"});
+  assertMatchPartial(triePtr, {"z"});
+
+  assertNotMatch(triePtr, {"x", "z", "+"});
+  assertNotMatch(triePtr, {"y", "z", "+"});
+  assertNotMatch(triePtr, {"w", "z", "+"});
+}
+
+TEST_CASE("Trie Building - Heavier Tree") {
+  TestASTProvider treeProvider;
+  auto trie = PatternConverter::convertASTToTrie(
+      treeProvider.balancedTree->getRoot());
+  auto triePtr = trie.get();
+
+  // x * y + x * z;
+  assertMatchFull(triePtr, {"x", "y", "*", "x", "z", "*", "+"});
+
+  assertMatchPartial(triePtr, {"x", "y", "*"});
+  assertMatchPartial(triePtr, {"x", "z", "*"});
+  assertMatchPartial(triePtr, {"x"});
+  assertMatchPartial(triePtr, {"y"});
+  assertMatchPartial(triePtr, {"z"});
+
+  assertNotMatch(triePtr, {"y", "x", "+"});
+  assertNotMatch(triePtr, {"x", "y", "*", "x", "+"});
+  assertNotMatch(triePtr, {"x", "y", "*", "z", "+"});
+  assertNotMatch(triePtr, {"y", "x", "z", "*", "+"});
+  assertNotMatch(triePtr, {"x", "x", "z", "*", "+"});
+}
