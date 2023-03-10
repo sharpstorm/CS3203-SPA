00eeab1beca081001024bcd782e22f2542bfa79c
Wei-Xinn <E0544325@u.nus.edu>
2023-03-10 14:01:13 +0800

    feat: add while and if pattern

--- Team30/Code30/src/spa/src/sp/extractor/concrete_extractors/PatternExtractor.cpp
+++ Team30/Code30/src/spa/src/sp/extractor/concrete_extractors/PatternExtractor.cpp
@@ -4,6 +4,7 @@
 #include <utility>
 
 #include "common/pattern/PatternConverter.h"
+#include "ExpressionVariableExtractor.h"
 
 using std::shared_ptr, std::move;
 
@@ -12,6 +13,21 @@ PatternExtractor::PatternExtractor(PkbWriter* writer) : pkbWriter(writer) {
 
 void PatternExtractor::visitAssign(AssignNode* node) {
   addPattern(node->getLineNumber(), node->getChild(1));
+}
+
+void PatternExtractor::visitWhile(WhileNode* node) {
+  ExpressionVariableExtractor variableExtractor;
+  node->accept(&variableExtractor);
+  for (string s : variableExtractor.getVariableSet()) {
+    pkbWriter->addWhilePattern(node->getLineNumber(), s);
+  }
+}
+void PatternExtractor::visitIf(IfNode* node) {
+  ExpressionVariableExtractor variableExtractor;
+  node->accept(&variableExtractor);
+  for (string s : variableExtractor.getVariableSet()) {
+    pkbWriter->addIfPattern(node->getLineNumber(), s);
+  }
 }
 
 void PatternExtractor::addPattern(int x, shared_ptr<IASTNode> node) {
--- Team30/Code30/src/spa/src/sp/extractor/concrete_extractors/PatternExtractor.h
+++ Team30/Code30/src/spa/src/sp/extractor/concrete_extractors/PatternExtractor.h
@@ -12,6 +12,8 @@ class PatternExtractor : public AbstractExtractor {
  public:
   explicit PatternExtractor(PkbWriter* pkbWriter);
   void visitAssign(AssignNode* node) override;
+  void visitWhile(WhileNode* node) override;
+  void visitIf(IfNode* node) override;
  private:
   void addPattern(int x, shared_ptr<IASTNode> node);
   PkbWriter* pkbWriter;
