d0ab14151c48f72ffc926be82458100cf689b7ff
Wei-Xinn <E0544325@u.nus.edu>
2023-03-14 14:20:59 +0800

    feat: add ConstantConstraint and OverrideConstraint

---
+++ Team30/Code30/src/spa/src/qps/common/constraint/OverrideConstraint.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Constraint.h"
+#include "qps/common/PQLQuerySynonym.h"
+#include "OverrideTransformer.h"
+class OverrideConstraint : virtual public Constraint {
+ private:
+  string synName;
+  OverrideTransformer overrideTransformer;
+ public:
+  OverrideConstraint(string synName, OverrideTransformer overrideTransformer) :
+  synName(synName), overrideTransformer(overrideTransformer) {}
+  bool applyConstraint(VariableTable varTable, OverrideTable* overrideTable) {
+    overrideTable->insert({synName, overrideTransformer});
+  };
+};
