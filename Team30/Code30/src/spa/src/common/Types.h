24e2e4ce3269882fa41a655a87ef31ec0626af32
ddx-510 <ddx0510@gmail.com>
2023-03-16 01:46:12 +0800

    refactor: Create IRef abstract class - t4

--- Team30/Code30/src/spa/src/common/Types.h
+++ Team30/Code30/src/spa/src/common/Types.h
@@ -5,8 +5,8 @@
 #include <utility>
 
 using std::pair;
-using std::unordered_set;
 using std::string;
+using std::unordered_set;
 
 enum class EntityType { None, Procedure, Variable, Constant };
 
@@ -15,32 +15,44 @@ enum class StmtType { None, Read, Print, Assign, Call, While, If };
 typedef string EntityValue;
 typedef int StmtValue;
 
-struct StmtRef {
+template <typename T, typename V>
+struct IRef {
+ public:
+  virtual ~IRef() {}
+  virtual bool isKnown() const = 0;
+  virtual V getValue() const = 0;
+  virtual T getType() const = 0;
+};
+
+struct StmtRef : IRef<StmtType, int> {
   StmtType type;
-  StmtValue lineNum;
-  const bool isKnown() const {
-    return lineNum != 0;
-  }
+  int lineNum;
+  StmtRef(StmtType type, int lineNum) : type(type), lineNum(lineNum) {}
+  bool isKnown() const override { return lineNum != 0; }
+  int getValue() const override { return lineNum; }
+  StmtType getType() const override { return type; }
 };
 
-struct EntityRef {
+struct EntityRef : IRef<EntityType, string> {
   EntityType type;
-  EntityValue name;
-  const bool isKnown() const {
-    return !name.empty();
-  }
+  string name;
+  explicit EntityRef(EntityType type) : type(type) {}
+  EntityRef(EntityType type, string name) : type(type), name(name) {}
+  bool isKnown() const override { return !name.empty(); }
+  string getValue() const override { return name; }
+  EntityType getType() const override { return type; }
 };
 
-template<class T1, class T2>
+template <class T1, class T2>
 struct std::hash<pair<T1, T2>> {
   std::size_t operator()(const pair<T1, T2> &pair) const {
     return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
   }
 };
-template<typename K, typename V>
+template <typename K, typename V>
 using pair_set = unordered_set<pair<K, V>>;
 
-template<typename T, typename U>
+template <typename T, typename U>
 struct QueryResult {
   unordered_set<T> firstArgVals;
   unordered_set<U> secondArgVals;
