ace3a51b7649b458806b55b05e1b94d63e59ec79
Wei-Xinn <E0544325@u.nus.edu>
2023-03-18 21:40:47 +0800

    feat: add VariableTableProxyBuilder

---
+++ Team30/Code30/src/spa/src/qps/common/constraint/VariableTableProxyBuilder.cpp
@@ -0,0 +1,17 @@
+#include "VariableTableProxyBuilder.h"
+
+void VariableTableProxyBuilder::joinSynonyms(PQLQuerySynonym *syn1,
+                                             PQLQuerySynonym *syn2) {
+  ufds.mergeSets(syn1, syn2);
+}
+
+void VariableTableProxyBuilder::VariableTableProxyBuilder::build() {
+  for (auto i = table->getProxyArray()->begin();
+  i != table->getProxyArray()->end(); i++) {
+    i->second = ufds.findSet(i->second);
+  }
+}
+
+VariableTable* VariableTableProxyBuilder::getTable() {
+  return table;
+};
---
+++ Team30/Code30/src/spa/src/qps/common/constraint/VariableTableProxyBuilder.h
@@ -0,0 +1,14 @@
+#pragma
+#include "SynonymUFDS.h"
+
+class VariableTableProxyBuilder {
+ private:
+  VariableTable* table;
+  SynonymUFDS ufds;
+
+ public:
+  VariableTableProxyBuilder(VariableTable* varTable) : table(varTable), ufds(SynonymUFDS(varTable)) {};
+  void joinSynonyms(PQLQuerySynonym* syn1, PQLQuerySynonym* syn2);
+  void build();
+  VariableTable* getTable();
+};
