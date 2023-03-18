dadb77e690f75fa6037cb9dab9f0c54fdc937f54
Wei-Xinn <E0544325@u.nus.edu>
2023-03-18 21:41:55 +0800

    feat: update QueryExectuor to using VariableTableProxyBuilder

--- Team30/Code30/src/spa/src/qps/executor/QueryExecutor.cpp
+++ Team30/Code30/src/spa/src/qps/executor/QueryExecutor.cpp
@@ -10,14 +10,15 @@ QueryExecutor::QueryExecutor(PkbQueryHandler* pkbQH):
 }
 
 SynonymResultTable *QueryExecutor::executeQuery(PQLQuery* query) {
-  // TODO(WeiXin): Move this if necessary
-  OverrideTable* overrideTable = new OverrideTable();
+  VariableTableProxyBuilder variableTableProxyBuilder(query->getVarTable());
+  OverrideTable overrideTable;
   for (const auto& con : query->getConstraints()) {
-    if (con->applyConstraint(query->getVarTable(), overrideTable)) {
+    if (con->applyConstraint(&variableTableProxyBuilder, &overrideTable)) {
       bool isBoolResult = query->getResultVariables()->empty();
       return new SynonymResultTable(isBoolResult, false);
     }
   }
+  variableTableProxyBuilder.build();
 
   QueryPlanPtr plan = planner.getExecutionPlan(query);
   return orchestrator.execute(plan.get());
