abb3a29ed1ac90d6453217a4ee1a5671182c2831
Wei-Xinn <E0544325@u.nus.edu>
2023-03-18 10:32:02 +0800

    chore: fix linting

--- Team30/Code30/src/spa/src/qps/clauses/WithClause.cpp
+++ Team30/Code30/src/spa/src/qps/clauses/WithClause.cpp
@@ -33,13 +33,14 @@ PQLQueryResult *WithClause::evaluateOn(PkbQueryHandler *pkbQueryHandler) {
 
 bool WithClause::isEmptyResult() {
   return (((leftArg->getAttribute() & INT_RETURN_MASK) > 0)
-  && ((leftArg->getAttribute() & INT_RETURN_MASK)> 0))
-  &&
-  ((leftArg->getSynType() != PQL_SYN_TYPE_STMT)
-  && (rightArg->getSynType() != PQL_SYN_TYPE_STMT));
+      && ((leftArg->getAttribute() & INT_RETURN_MASK)> 0))
+      &&
+          ((leftArg->getSynType() != PQL_SYN_TYPE_STMT)
+              && (rightArg->getSynType() != PQL_SYN_TYPE_STMT));
 }
 
-void WithClause::evaluateOnIntAttributes(PQLQueryResult *result, PkbQueryHandler *pkbQueryHandler) {
+void WithClause::evaluateOnIntAttributes(PQLQueryResult *result,
+                                         PkbQueryHandler *pkbQueryHandler) {
   StmtType leftType = PKBTypeAdapter::convertPQLSynonymToStmt(leftArg->getSynType());
   StmtType rightType = PKBTypeAdapter::convertPQLSynonymToStmt(rightArg->getSynType());
   unordered_set<int> set1 = pkbQueryHandler->getStatementsOfType(leftType);
@@ -54,23 +55,28 @@ void WithClause::evaluateOnIntAttributes(PQLQueryResult *result, PkbQueryHandler
   result->add(leftArg->getSynName(), rightArg->getSynName(), queryResult);
 }
 
-void WithClause::evaluateOnStringAttributes(PQLQueryResult *result, PkbQueryHandler *pkbQueryHandler) {
+void WithClause::evaluateOnStringAttributes(PQLQueryResult *result,
+                                            PkbQueryHandler *pkbQueryHandler) {
   SynToStmtMap map1;
   SynToStmtMap map2;
   bool isLeftDefault = populateMap(leftArg->getSynType(), &map1, pkbQueryHandler);
   bool isRightDefault = populateMap(rightArg->getSynType(), &map2, pkbQueryHandler);
 
   if (isLeftDefault && isRightDefault) {
-    auto queryResult = crossMaps<string, string>(&map1, &map2, keyExtractor, keyExtractor);
+    auto queryResult = crossMaps<string, string>
+        (&map1, &map2, keyExtractor,keyExtractor);
     result->add(leftArg->getSynName(), rightArg->getSynName(), queryResult);
   } else if (!isLeftDefault && !isRightDefault) {
-    auto queryResult = crossMaps<int, int>(&map1, &map2, valueExtractor, valueExtractor);
+    auto queryResult = crossMaps<int, int>
+        (&map1, &map2, valueExtractor, valueExtractor);
     result->add(leftArg->getSynName(), rightArg->getSynName(), queryResult);
   } else if (isLeftDefault) {
-    auto queryResult = crossMaps<string, int>(&map1, &map2, keyExtractor, valueExtractor);
+    auto queryResult = crossMaps<string, int>
+        (&map1, &map2, keyExtractor, valueExtractor);
     result->add(leftArg->getSynName(), rightArg->getSynName(), queryResult);
   } else {
-    auto queryResult = crossMaps<int, string>(&map1, &map2, valueExtractor, keyExtractor);
+    auto queryResult = crossMaps<int, string>
+        (&map1, &map2, valueExtractor, keyExtractor);
     result->add(leftArg->getSynName(), rightArg->getSynName(), queryResult);
   }
   for (auto &it1 : map1) {
@@ -95,32 +101,35 @@ void WithClause::queryPkbForAttribute(PkbQueryHandler *pkbQueryHandler,
   }
 }
 
-constexpr PKBAttributeQuerier CallsQuerier = [](PkbQueryHandler *pkbQueryHandler,
-                                                const int &stmt) {
-  return pkbQueryHandler->getCalledDeclaration(stmt);
-};
+constexpr PKBAttributeQuerier CallsQuerier =
+    [](PkbQueryHandler *pkbQueryHandler, const int &stmt) {
+      return pkbQueryHandler->getCalledDeclaration(stmt);
+    };
 
-constexpr PKBAttributeQuerier ReadQuerier = [](PkbQueryHandler *pkbQueryHandler,
-                                               const int &stmt) {
-  return pkbQueryHandler->getReadDeclarations(stmt);
-};
+constexpr PKBAttributeQuerier ReadQuerier =
+    [](PkbQueryHandler *pkbQueryHandler, const int &stmt) {
+      return pkbQueryHandler->getReadDeclarations(stmt);
+    };
 
-constexpr PKBAttributeQuerier PrintQuerier = [](PkbQueryHandler *pkbQueryHandler,
-                                                const int &stmt) {
-  return pkbQueryHandler->getPrintDeclarations(stmt);
-};
+constexpr PKBAttributeQuerier PrintQuerier =
+    [](PkbQueryHandler *pkbQueryHandler, const int &stmt) {
+      return pkbQueryHandler->getPrintDeclarations(stmt);
+    };
 
 bool WithClause::populateMap(PQLSynonymType type, SynToStmtMap *map,
                              PkbQueryHandler *pkbQueryHandler) {
   switch (type) {
     case PQL_SYN_TYPE_CALL:
-      queryPkbForAttribute<CallsQuerier, PQL_SYN_TYPE_CALL>(pkbQueryHandler, map);
+      queryPkbForAttribute<CallsQuerier,
+                           PQL_SYN_TYPE_CALL>(pkbQueryHandler, map);
       return false;
     case PQL_SYN_TYPE_READ:
-      queryPkbForAttribute<ReadQuerier, PQL_SYN_TYPE_READ>(pkbQueryHandler, map);
+      queryPkbForAttribute<ReadQuerier,
+                           PQL_SYN_TYPE_READ>(pkbQueryHandler, map);
       return false;
     case PQL_SYN_TYPE_PRINT:
-      queryPkbForAttribute<PrintQuerier, PQL_SYN_TYPE_PRINT>(pkbQueryHandler, map);
+      queryPkbForAttribute<PrintQuerier,
+                           PQL_SYN_TYPE_PRINT>(pkbQueryHandler, map);
       return false;
     default:
       break;
--- Team30/Code30/src/spa/src/qps/clauses/WithClause.h
+++ Team30/Code30/src/spa/src/qps/clauses/WithClause.h
@@ -33,14 +33,16 @@ class WithClause: public Clause {
 
  private:
   bool isEmptyResult();
-  void evaluateOnIntAttributes(PQLQueryResult *result, PkbQueryHandler *pkbQueryHandler);
-  void evaluateOnStringAttributes(PQLQueryResult *result, PkbQueryHandler *pkbQueryHandler);
+  void evaluateOnIntAttributes(PQLQueryResult *result,
+                               PkbQueryHandler *pkbQueryHandler);
+  void evaluateOnStringAttributes(PQLQueryResult *result
+                                  PkbQueryHandler *pkbQueryHandler);
 
   template <class R1, class R2, class U, class V, class T1, class T2>
   pair_set<R1, R2> crossMaps(SynToStmtMap* map1,
-                                SynToStmtMap* map2,
-                                SynStmtMapExtractor<T1, U, V> leftExtractor,
-                                SynStmtMapExtractor<T2, U, V> rightExtractor) {
+                             SynToStmtMap* map2,
+                             SynStmtMapExtractor<T1, U, V> leftExtractor,
+                             SynStmtMapExtractor<T2, U, V> rightExtractor) {
     pair_set<R1, R2> result;
     for (auto it1 = map1->begin(); it1 != map1->end(); it1++) {
       auto map2FindIterator = map2->find(it1->first);
@@ -49,14 +51,16 @@ class WithClause: public Clause {
       }
 
       crossProduct(leftExtractor(&(it1->first), &(it1->second)),
-                   rightExtractor(&(map2FindIterator->first), &(map2FindIterator)->second),
+                   rightExtractor(&(map2FindIterator->first),
+                                  &(map2FindIterator)->second),
                    &result);
     }
 
     return result;
   }
 
-  void crossProduct(const StmtList* set1, const StmtList* set2, pair_set<int, int>* output) {
+  void crossProduct(const StmtList* set1, const StmtList* set2,
+                    pair_set<int, int>* output) {
     for (auto it1 = set1->begin(); it1 != set1->end(); it1++) {
       for (auto it2 = set2->begin(); it2 != set2->end(); it2++) {
         output->insert({*it1, *it2});
@@ -64,19 +68,22 @@ class WithClause: public Clause {
     }
   }
 
-  void crossProduct(const string* set1, const StmtList* set2, pair_set<string, int>* output) {
+  void crossProduct(const string* set1, const StmtList* set2,
+                    pair_set<string, int>* output) {
     for (auto it = set2->begin(); it != set2->end(); it++) {
       output->insert({*set1, *it});
     }
   }
 
-  void crossProduct(const StmtList* set1, const string* set2, pair_set<int, string>* output) {
+  void crossProduct(const StmtList* set1, const string* set2,
+                    pair_set<int, string>* output) {
     for (auto it = set1->begin(); it != set1->end(); it++) {
       output->insert({*it, *set2});
     }
   }
 
-  void crossProduct(const string* set1, const string* set2, pair_set<string, string>* output) {
+  void crossProduct(const string* set1, const string* set2, pair_set<string,
+                                                                     string>* output) {
     output->insert({*set1, *set2});
   }
   SynonymList getUsedSynonyms() override;
