1729b973c1ecc36917597dba162a039fd17eb4b8
sharpstorm <chimchar17@gmail.com>
2023-03-19 22:28:53 +0800

    fix: cat 2 and cat 3 with clause interaction

--- Team30/Code30/src/spa/src/qps/common/constraint/SynonymProxyBuilder.cpp
+++ Team30/Code30/src/spa/src/qps/common/constraint/SynonymProxyBuilder.cpp
@@ -6,13 +6,38 @@ void SynonymProxyBuilder::joinSynonyms(const PQLSynonymName &syn1,
 }
 
 void SynonymProxyBuilder::SynonymProxyBuilder::build() {
-  ProxyMap* proxyMap = table->getProxyMap();
+  ProxyMap* proxyMap = variableTable->getProxyMap();
   for (auto i = proxyMap->begin(); i != proxyMap->end(); i++) {
     PQLQuerySynonym* newPtr = ufds.getSetValue(i->first);
     i->second.updatePointer(newPtr);
   }
 }
 
+bool SynonymProxyBuilder::resolveOverrideMerging(OverrideTable *overrideTable) {
+  ProxyMap* proxyMap = variableTable->getProxyMap();
+  for (auto i = proxyMap->begin(); i != proxyMap->end(); i++) {
+    PQLQuerySynonym* mappedSynonym = ufds.getSetValue(i->first);
+    PQLSynonymName newName = mappedSynonym->getName();
+    if (i->first == newName) {
+      continue;
+    }
+
+    auto oldTransformer = overrideTable->find(i->first);
+    if (oldTransformer == overrideTable->end()) {
+      continue;
+    }
+
+    auto newTransformer = overrideTable->find(newName);
+    if (newTransformer == overrideTable->end()) {
+      overrideTable->emplace(newName, oldTransformer->second);
+      continue;
+    }
+    return false;
+  }
+
+  return true;
+}
+
 VariableTable* SynonymProxyBuilder::getTable() {
-  return table;
+  return variableTable;
 }
--- Team30/Code30/src/spa/src/qps/common/constraint/SynonymProxyBuilder.h
+++ Team30/Code30/src/spa/src/qps/common/constraint/SynonymProxyBuilder.h
@@ -3,22 +3,24 @@
 #include <memory>
 
 #include "SynonymUFDS.h"
+#include "OverrideTable.h"
 
 using std::unique_ptr;
 
 class SynonymProxyBuilder {
  private:
-  VariableTable* table;
+  VariableTable* variableTable;
   SynonymUFDS ufds;
 
  public:
   explicit SynonymProxyBuilder(VariableTable* varTable) :
-      table(varTable), ufds(SynonymUFDS(varTable)) {}
+      variableTable(varTable), ufds(SynonymUFDS(varTable)) {}
   void joinSynonyms(const PQLSynonymName &syn1,
                     const PQLSynonymName &syn2);
   void build();
+  bool resolveOverrideMerging(OverrideTable* overrideTable);
 
   VariableTable* getTable();
 };
 
-typedef unique_ptr<SynonymProxyBuilder> VariableTableProxyBuilderPtr;
+typedef unique_ptr<SynonymProxyBuilder> SynonymProxyBuilderPtr;
