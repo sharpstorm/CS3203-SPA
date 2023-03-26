da80917a6095c40725f94156b5628b36cfd7c1b2
lingshanng <nglingshan456@gmail.com>
2023-03-26 13:38:46 +0800

    feat: add IndexTableManager

---
+++ Team30/Code30/src/spa/src/pkb/storage/IndexTableManager.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <unordered_set>
+
+#include "common/Types.h"
+#include "pkb/storage/tables/HashKeyTable.h"
+#include "pkb/storage/tables/IBaseTable.h"
+
+using std::unordered_set;
+
+template <typename Value>
+class IndexTableManager {
+ private:
+  IBaseTable<EntityIdx, Value> *table;
+  IBaseTable<Value, EntityIdx> *reverseTable;
+  unordered_set<Value> allValues;
+  EntityIdx index;
+
+ public:
+  IndexTableManager(IBaseTable<EntityIdx, Value> *table,
+                    IBaseTable<Value, EntityIdx> *reverseTable)
+      : table(table), reverseTable(reverseTable), index(0){};
+
+  EntityIdx insert(Value value) {
+    index++;
+    table->set(index, value);
+    reverseTable->set(value, index);
+    allValues.insert(value);
+    return index;
+  }
+
+  Value getValueByIdx(const EntityIdx &idx) { return table->get(idx); }
+
+  EntityIdx getIdxOfValue(const Value &value) {
+    return reverseTable->get(value);
+  }
+
+  unordered_set<Value> getAllValues() { return allValues; }
+};
