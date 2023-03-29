cca5a6fa1d40e3a915a1beaca03811837b759de6
ddx-510 <ddx0510@gmail.com>
2023-03-29 15:58:06 +0800

    fix: fix lint

--- Team30/Code30/src/spa/src/pkb/storage/tables/IBaseSetTable.h
+++ Team30/Code30/src/spa/src/pkb/storage/tables/IBaseSetTable.h
@@ -6,11 +6,10 @@ using std::set;
 /**
  * Table that maps item of type K to a set of type V items.
  */
-template<typename K, typename V>
+template <typename K, typename V>
 class IBaseSetTable {
  public:
   virtual ~IBaseSetTable() {}
   virtual void insert(K key, V value) = 0;
   virtual const set<V>& get(K key) const = 0;
-
 };
--- Team30/Code30/src/spa/src/pkb/storage/tables/IBaseTable.h
+++ Team30/Code30/src/spa/src/pkb/storage/tables/IBaseTable.h
@@ -3,11 +3,10 @@
 /**
  * Table that maps item of type K to an item of type V.
  */
-template<typename K, typename V>
+template <typename K, typename V>
 class IBaseTable {
  public:
   virtual ~IBaseTable() {}
   virtual void insert(K key, V value) = 0;
   virtual const V& get(K key) const = 0;
-
 };
