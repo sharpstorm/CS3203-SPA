69dd758b4dd25ca4bc44eab7330bfd751600232c
Wei-Xinn <E0544325@u.nus.edu>
2023-03-29 13:04:01 +0800

    feat: change FollowsExtractor to add transitive follows relations

--- Team30/Code30/src/spa/src/sp/extractor/concrete_extractors/FollowsExtractor.h
+++ Team30/Code30/src/spa/src/sp/extractor/concrete_extractors/FollowsExtractor.h
@@ -1,13 +1,29 @@
 #pragma once
 
+#include <stack>
 #include "sp/extractor/AbstractExtractor.h"
+
+using std::stack;
+
+typedef int LineNumber;
+typedef vector<LineNumber> LineNumbers;
+typedef shared_ptr<LineNumbers> LineNumbersPtr;
 
 class FollowsExtractor : public AbstractExtractor {
  public:
   explicit FollowsExtractor(PkbWriter* pkbWriter);
+  void visitRead(ReadNode* node) override;
+  void visitPrint(PrintNode* node) override;
+  void visitAssign(AssignNode* node) override;
+  void visitWhile(WhileNode* node) override;
+  void visitIf(IfNode* node) override;
+  void visitCall(CallNode* node) override;
   void visitStmtList(StatementListNode* node) override;
+  void leaveStmtList(StatementListNode* node) override;
 
  private:
-  void addFollowsRelation(int x, int y);
+  void addFollowsRelation(LineNumber x, LineNumber y);
+  void processStatement(StatementASTNode* node);
+  stack<LineNumbersPtr> stmtLstCache;
   PkbWriter* pkbWriter;
 };
