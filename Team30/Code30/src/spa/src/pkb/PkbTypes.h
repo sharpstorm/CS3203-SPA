a2beba33271901aa337e520fb2822a915042e30d
lingshanng <nglingshan456@gmail.com>
2023-03-30 22:18:08 +0800

    chore: revert formatting

--- Team30/Code30/src/spa/src/pkb/PkbTypes.h
+++ Team30/Code30/src/spa/src/pkb/PkbTypes.h
@@ -7,35 +7,35 @@
 using std::function;
 
 namespace pkb {
-// predicate
-template <typename T>
-using Predicate = function<bool(T const &)>;
+  // predicate
+  template <typename T>
+  using Predicate = function<bool(T const &)>;
 
-// validators
-template <typename Value, typename Type>
-using ArgValidator = bool (*)(const IRef<Value, Type> *ref);
+  // validators
+  template <typename Value, typename Type>
+  using ArgValidator = bool (*)(const IRef<Value, Type> *ref);
 
-typedef ArgValidator<StmtValue, StmtType> StmtRefValidator;
-typedef ArgValidator<EntityValue, EntityType> EntityRefValidator;
+  typedef ArgValidator<StmtValue, StmtType> StmtRefValidator;
+  typedef ArgValidator<EntityValue, EntityType> EntityRefValidator;
 
-template <typename Value, typename Type>
-constexpr ArgValidator<Value, Type> defaultValidator =
-    [](const IRef<Value, Type> *arg) { return true; };
+  template <typename Value, typename Type>
+  constexpr ArgValidator<Value, Type> defaultValidator =
+      [](const IRef<Value, Type> *arg) { return true; };
 
-// transformers
-template <typename Value, typename Type>
-using ArgTransformer = IRef<Value, Type> *(*)(IRef<Value, Type> *ref);
+  // transformers
+  template <typename Value, typename Type>
+  using ArgTransformer = IRef<Value, Type> *(*)(IRef<Value, Type> *ref);
 
-template <typename Value, typename Type>
-constexpr ArgTransformer<Value, Type> defaultTransformer =
-    [](IRef<Value, Type> *arg) { return arg; };
+  template <typename Value, typename Type>
+  constexpr ArgTransformer<Value, Type> defaultTransformer =
+      [](IRef<Value, Type> *arg) { return arg; };
 
-// function
-template <typename K, typename V>
-using Function = function<void(const K &key, const V &value)>;
-// using Function = void (*)(const K &key, const V &value);
+  // function
+  template <typename K, typename V>
+  using Function = function<void(const K &key, const V &value)>;
+  // using Function = void (*)(const K &key, const V &value);
 
-template <typename K, typename V>
-using Terminator = function<bool(const K &key, const V &value)>;
+  template <typename K, typename V>
+  using Terminator = function<bool(const K &key, const V &value)>;
 
 }  // namespace pkb
