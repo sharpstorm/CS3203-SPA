d04ce73551323370426c218d597020da43822d06
lingshanng <nglingshan456@gmail.com>
2023-04-01 23:02:07 +0800

    tests: modifies test

--- Team30/Code30/src/unit_testing/src/pkb/queryHandlers/ModifiesQueryHandlerTest.cpp
+++ Team30/Code30/src/unit_testing/src/pkb/queryHandlers/ModifiesQueryHandlerTest.cpp
@@ -76,7 +76,8 @@ struct modifiesTest {
       make_unique<EntityPredicateFactory>();
   unique_ptr<PkbStmtEntQueryInvoker> stmtEntInvoker =
       make_unique<PkbStmtEntQueryInvoker>(
-          structureProvider.get(), stmtPredFactory.get(), entPredFactory.get());
+          structureProvider.get(), entityProvider.get(), stmtPredFactory.get(),
+          entPredFactory.get());
   unique_ptr<PkbEntEntQueryInvoker> entEntInvoker =
       make_unique<PkbEntEntQueryInvoker>(entityProvider.get(),
                                          entPredFactory.get());
@@ -134,9 +135,9 @@ TEST_CASE("ModifiesQueryHandler Modifies(stmtNum, _)") {
 
   auto result = *test.query({StmtType::None, 1}, {EntityType::Wildcard, ""});
   REQUIRE(result.isEmpty == false);
-  REQUIRE(result.firstArgVals == unordered_set<int>({1}));
-  REQUIRE(result.secondArgVals == unordered_set<string>({"x", "y"}));
-  REQUIRE(result.pairVals == pair_set<int, string>({{1, "x"}, {1, "y"}}));
+
+  auto result2 = *test.query({StmtType::None, 3}, {EntityType::Wildcard, ""});
+  REQUIRE(result2.isEmpty == true);
 }
 
 TEST_CASE("ModifiesQueryHandler Modifies(stmtNum, constant)") {
@@ -266,9 +267,6 @@ TEST_CASE("ModifiesQueryHandler Modifies(statement, _)") {
 
   REQUIRE(result1.isEmpty == false);
   REQUIRE(result1.firstArgVals == unordered_set<int>({1, 4, 6}));
-  REQUIRE(result1.secondArgVals == unordered_set<string>({"x", "y", "z"}));
-  REQUIRE(result1.pairVals ==
-          pair_set<int, string>({{1, "x"}, {1, "z"}, {4, "x"}, {6, "y"}}));
 }
 
 TEST_CASE("ModifiesQueryHandler call statement") {
@@ -283,15 +281,14 @@ TEST_CASE("ModifiesQueryHandler call statement") {
 
   // arg1 known
   auto result1 = *test.query({StmtType::None, 8}, {EntityType::Wildcard, ""});
-  REQUIRE(result1.pairVals == pair_set<int, string>({{8, "x"}, {8, "y"}}));
+  REQUIRE(result1.isEmpty == false);
   // arg2 known
   auto result2 = *test.query({StmtType::Call, 0}, {EntityType::Variable, ""});
 
   REQUIRE(result2.pairVals == pair_set<int, string>({{8, "x"}, {8, "y"}}));
   // Both args unknown
   auto result3 = *test.query({StmtType::None, 0}, {EntityType::Wildcard, ""});
-  REQUIRE(result3.pairVals ==
-          pair_set<int, string>({{8, "x"}, {8, "y"}, {1, "z"}}));
+  REQUIRE(result3.firstArgVals == unordered_set<int>({1, 8}));
 }
 
 /** Modifies(EntityRef, EntityRef) */
@@ -376,10 +373,6 @@ TEST_CASE("ModifiesQueryHandler Modifies(type, type)") {
       *test.query({EntityType::Procedure, ""}, {EntityType::Wildcard, ""});
   REQUIRE(result1.isEmpty == false);
   REQUIRE(result1.firstArgVals == unordered_set<string>({"main", "foo"}));
-  REQUIRE(result1.secondArgVals == unordered_set<string>({"x", "y", "z"}));
-  REQUIRE(result1.pairVals ==
-          pair_set<string, string>(
-              {{"main", "x"}, {"main", "y"}, {"foo", "z"}, {"foo", "y"}}));
 
   // invalid arg1
   auto result2 =
