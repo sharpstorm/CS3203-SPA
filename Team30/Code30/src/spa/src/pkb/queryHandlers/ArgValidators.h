7cbda275e614ac3620d5a646d3afd74715413994
lingshanng <nglingshan456@gmail.com>
2023-04-01 16:49:11 +0800

    test: add tests for follows

--- Team30/Code30/src/spa/src/pkb/queryHandlers/ArgValidators.h
+++ Team30/Code30/src/spa/src/pkb/queryHandlers/ArgValidators.h
@@ -1,55 +1,54 @@
 #pragma once
 
-#include "pkb/PkbTypes.h"
 #include "common/Types.h"
+#include "pkb/PkbTypes.h"
 
 using pkb::StmtRefValidator, pkb::EntityRefValidator;
 
-constexpr StmtRefValidator modifiesLeftArgValidator = [](
-    const IRef<StmtValue, StmtType> *arg) {
-  return arg->getType() != StmtType::Print;
-};
+constexpr StmtRefValidator modifiesLeftArgValidator =
+    [](const IRef<StmtValue, StmtType> *arg) {
+      return !arg->isType(StmtType::Print) && !arg->isType(StmtType::Wildcard);
+    };
 
-constexpr StmtRefValidator usesLeftArgValidator = [](
-    const IRef<StmtValue, StmtType> *arg) {
-  return arg->getType() != StmtType::Read;
-};
+constexpr StmtRefValidator usesLeftArgValidator =
+    [](const IRef<StmtValue, StmtType> *arg) {
+      return !arg->isType(StmtType::Read) && !arg->isType(StmtType::Wildcard);
+    };
 
 // for modifies and uses
-constexpr EntityRefValidator varRightArgValidator = [](
-    const IRef<EntityValue, EntityType> *arg) {
-  return arg->getType() == EntityType::None
-      || arg->getType() == EntityType::Variable;
-};
+constexpr EntityRefValidator varRightArgValidator =
+    [](const IRef<EntityValue, EntityType> *arg) {
+      return arg->getType() == EntityType::None ||
+             arg->getType() == EntityType::Variable;
+    };
 
 // for modifiesP and usesP
-constexpr EntityRefValidator procLeftArgValidator = [](
-    const IRef<EntityValue, EntityType> *arg) {
-  return arg->getType() == EntityType::Procedure
-      || (arg->getType() == EntityType::None && arg->isKnown());
-};
+constexpr EntityRefValidator procLeftArgValidator =
+    [](const IRef<EntityValue, EntityType> *arg) {
+      return arg->getType() == EntityType::Procedure ||
+             (arg->getType() == EntityType::None && arg->isKnown());
+    };
 
-constexpr EntityRefValidator callsArgValidator = [](
-    const IRef<EntityValue, EntityType> *arg) {
-  return arg->getType() == EntityType::None
-      || arg->getType() == EntityType::Procedure;
-};
+constexpr EntityRefValidator callsArgValidator =
+    [](const IRef<EntityValue, EntityType> *arg) {
+      return arg->getType() == EntityType::None ||
+             arg->getType() == EntityType::Procedure;
+    };
 
-constexpr StmtRefValidator ifLeftArgValidator = [](
-    const IRef<StmtValue, StmtType> *arg) {
-  return arg->getType() == StmtType::If
-      || (arg->getType() == StmtType::None && arg->isKnown());
-};
+constexpr StmtRefValidator ifLeftArgValidator =
+    [](const IRef<StmtValue, StmtType> *arg) {
+      return arg->getType() == StmtType::If ||
+             (arg->getType() == StmtType::None && arg->isKnown());
+    };
 
-constexpr StmtRefValidator whileLeftArgValidator = [](
-    const IRef<StmtValue, StmtType> *arg) {
-  return arg->getType() == StmtType::While
-      || (arg->getType() == StmtType::None && arg->isKnown());
-};
+constexpr StmtRefValidator whileLeftArgValidator =
+    [](const IRef<StmtValue, StmtType> *arg) {
+      return arg->getType() == StmtType::While ||
+             (arg->getType() == StmtType::None && arg->isKnown());
+    };
 
-constexpr StmtRefValidator parentLeftArgValidator = [](
-    const IRef<StmtValue, StmtType> *arg) {
-  return arg->getType() == StmtType::While
-         || arg->getType() == StmtType::If
-         || arg->getType() == StmtType::None;
-};
+constexpr StmtRefValidator parentLeftArgValidator =
+    [](const IRef<StmtValue, StmtType> *arg) {
+      return arg->getType() == StmtType::While ||
+             arg->getType() == StmtType::If || arg->getType() == StmtType::None;
+    };
