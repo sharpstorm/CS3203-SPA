f6f9d5c105ce716f56db46329f722e6b12e54c96
sharpstorm <chimchar17@gmail.com>
2023-04-01 23:28:42 +0800

    chore: refactor AffectsT DFS

--- Team30/Code30/src/spa/src/qps/cfg/cfg_querier/CFGAffectsTQuerier.h
+++ Team30/Code30/src/spa/src/qps/cfg/cfg_querier/CFGAffectsTQuerier.h
@@ -26,6 +26,14 @@ class CFGAffectsTQuerier : public ICFGClauseQuerier,
                                                   usesGetter>> {
   using ConcreteAffectsQuerier = CFGAffectsQuerier<ClosureType, typePredicate,
                                                    modifiesGetter, usesGetter>;
+  template <class T>
+  using DFSResultCallback = bool(*)(T* dfsClosure,
+                                    const StmtValue& start,
+                                    const StmtValue &stmt);
+  template <class T, class U>
+  using DFSStepGetter = U(*)(T* dfsClosure, const StmtValue &stmt);
+  typedef unordered_set<StmtValue> AffectsResult;
+  typedef vector<AffectsResult> AffectsResultList;
  public:
   explicit CFGAffectsTQuerier(CFG *cfg, const ClosureType &closure);
 
@@ -44,9 +52,45 @@ class CFGAffectsTQuerier : public ICFGClauseQuerier,
   ConcreteAffectsQuerier affectsQuerier;
   const ClosureType &closure;
 
+  struct BoolDFSClosure {
+    StmtTransitiveResult *output;
+    ConcreteAffectsQuerier affectsQuerier;
+    StmtValue target;
+  };
+
+  struct SingleSynDFSClosure {
+    StmtTransitiveResult *output;
+    ConcreteAffectsQuerier affectsQuerier;
+  };
+
+  struct AllDFSClosure {
+    StmtTransitiveResult *output;
+    ConcreteAffectsQuerier affectsQuerier;
+    const AffectsResultList *affectsResults;
+    CFG* cfg;
+  };
+
   bool validateArg(const StmtValue &arg);
-  void queryForward(StmtTransitiveResult *resultOut,
-                    const StmtValue &start);
+
+  template <class DFSClosureType, class DFSStepType>
+  void runAffectsTDFS(DFSResultCallback<DFSClosureType> callback,
+                      DFSStepGetter<DFSClosureType, DFSStepType> stepGetter,
+                      DFSClosureType *dfsClosure,
+                      const StmtValue &start);
+
+  template <class T>
+  static constexpr DFSStepGetter<T, AffectsResult> affectsQueryFrom =
+      [](T* dfsClosure, const StmtValue &stmt) {
+        return dfsClosure->affectsQuerier.queryFrom(stmt, StmtType::Assign)
+            .secondArgVals;
+      };
+
+  template <class T>
+  static constexpr DFSStepGetter<T, AffectsResult> affectsQueryTo =
+      [](T* dfsClosure, const StmtValue &stmt) {
+        return dfsClosure->affectsQuerier.queryTo(StmtType::Assign, stmt)
+            .firstArgVals;
+      };
 };
 
 template<
@@ -73,29 +117,20 @@ queryBool(const StmtValue &arg0, const StmtValue &arg1) {
     return result;
   }
 
-  vector<StmtValue> dfsStack;
-  BitField seen(cfg->getNodeCount());
-  dfsStack.push_back(arg0);
-  while (!dfsStack.empty()) {
-    StmtValue curStmt = dfsStack.back();
-    dfsStack.pop_back();
+  BoolDFSClosure dfsClosure{&result, affectsQuerier, arg1};
+  constexpr DFSResultCallback<BoolDFSClosure> resultHandler =
+      [](BoolDFSClosure* dfsClosure,
+         const StmtValue& start,
+         const StmtValue &stmt) -> bool {
+        if (stmt == dfsClosure->target) {
+          dfsClosure->output->add(start, stmt);
+          return false;
+        }
+        return true;
+      };
 
-    StmtTransitiveResult affectsResult = affectsQuerier
-        .queryFrom(curStmt, StmtType::Assign);
-    for (const StmtValue &stmt : affectsResult.secondArgVals) {
-      CFGNode nextNode = cfg->toCFGNode(stmt);
-      if (seen.isSet(nextNode)) {
-        continue;
-      }
-      if (stmt == arg1) {
-        result.add(arg0, arg1);
-        return result;
-      }
-
-      seen.set(nextNode);
-      dfsStack.push_back(stmt);
-    }
-  }
+  runAffectsTDFS(resultHandler, affectsQueryFrom<BoolDFSClosure>,
+                 &dfsClosure, arg0);
   return result;
 }
 
@@ -109,7 +144,17 @@ StmtTransitiveResult CFGAffectsTQuerier<ClosureType, typePredicate,
     const StmtValue &arg0, const StmtType &type1) {
   StmtTransitiveResult result;
 
-  queryForward(&result, arg0);
+  SingleSynDFSClosure dfsClosure{&result, affectsQuerier};
+  constexpr DFSResultCallback<SingleSynDFSClosure> resultHandler =
+      [](SingleSynDFSClosure* result,
+         const StmtValue& start,
+         const StmtValue &stmt) -> bool {
+        result->output->add(start, stmt);
+        return true;
+      };
+
+  runAffectsTDFS(resultHandler, affectsQueryFrom<SingleSynDFSClosure>,
+                 &dfsClosure, arg0);
   return result;
 }
 
@@ -126,25 +171,17 @@ queryTo(const StmtType &type0, const StmtValue &arg1) {
     return result;
   }
 
-  vector<StmtValue> dfsStack;
-  BitField seen(cfg->getNodeCount());
-  dfsStack.push_back(arg1);
-  while (!dfsStack.empty()) {
-    StmtValue curStmt = dfsStack.back();
-    dfsStack.pop_back();
+  SingleSynDFSClosure dfsClosure{&result, affectsQuerier};
+  constexpr DFSResultCallback<SingleSynDFSClosure> resultHandler =
+      [](SingleSynDFSClosure* result,
+         const StmtValue& start,
+         const StmtValue &stmt) -> bool {
+        result->output->add(stmt, start);
+        return true;
+      };
 
-    StmtTransitiveResult affectsResult = affectsQuerier.queryTo(type0, curStmt);
-    for (const StmtValue &stmt : affectsResult.firstArgVals) {
-      CFGNode nextNode = cfg->toCFGNode(stmt);
-      if (seen.isSet(nextNode)) {
-        continue;
-      }
-      result.add(stmt, arg1);
-      seen.set(nextNode);
-      dfsStack.push_back(stmt);
-    }
-  }
-
+  runAffectsTDFS(resultHandler, affectsQueryTo<SingleSynDFSClosure>,
+                 &dfsClosure, arg1);
   return result;
 }
 
@@ -158,65 +195,31 @@ void CFGAffectsTQuerier<ClosureType, typePredicate,
 queryAll(StmtTransitiveResult *resultOut,
          const StmtType &type0,
          const StmtType &type1) {
-  vector<unordered_set<StmtValue>> resultStmts;
-  for (int start = 0; start < cfg->getNodeCount(); start++) {
-    auto result = affectsQuerier.queryFrom(cfg->fromCFGNode(start), type1);
+  AffectsResultList resultStmts;
+  for (CFGNode start = 0; start < cfg->getNodeCount(); start++) {
+    StmtTransitiveResult result = affectsQuerier.queryFrom(
+        cfg->fromCFGNode(start), type1);
     resultStmts.push_back(result.secondArgVals);
   }
 
-  for (int start = 0; start < cfg->getNodeCount(); start++) {
+  AllDFSClosure dfsClosure{resultOut, affectsQuerier, &resultStmts, cfg};
+  constexpr DFSResultCallback<AllDFSClosure> resultHandler =
+      [](AllDFSClosure* closure,
+         const StmtValue& start,
+         const StmtValue &stmt) -> bool {
+        closure->output->add(start, stmt);
+        return true;
+      };
+
+  constexpr DFSStepGetter<AllDFSClosure, AffectsResult> stepGetter =
+      [](AllDFSClosure* dfsClosure, const StmtValue &stmt) {
+        CFGNode nodeId = dfsClosure->cfg->toCFGNode(stmt);
+        return dfsClosure->affectsResults->at(nodeId);
+      };
+
+  for (CFGNode start = 0; start < cfg->getNodeCount(); start++) {
     StmtValue startStmt = cfg->fromCFGNode(start);
-    vector<CFGNode> dfsStack;
-    BitField seen(cfg->getNodeCount());
-    dfsStack.push_back(start);
-    while (!dfsStack.empty()) {
-      CFGNode curNode = dfsStack.back();
-      dfsStack.pop_back();
-
-      for (const StmtValue &stmt : resultStmts.at(curNode)) {
-        CFGNode nextNode = cfg->toCFGNode(stmt);
-        if (seen.isSet(nextNode)) {
-          continue;
-        }
-        seen.set(nextNode);
-        resultOut->add(startStmt, stmt);
-        dfsStack.push_back(nextNode);
-      }
-    }
-  }
-}
-
-template<
-    class ClosureType,
-    StmtTypePredicate<ClosureType> typePredicate,
-    ModifiesGetter<ClosureType> modifiesGetter,
-    UsesGetter<ClosureType> usesGetter>
-void CFGAffectsTQuerier<ClosureType, typePredicate,
-                        modifiesGetter, usesGetter>::queryForward(
-    StmtTransitiveResult *resultOut, const StmtValue &start) {
-  if (!validateArg(start)) {
-    return;
-  }
-
-  vector<StmtValue> dfsStack;
-  BitField seen(cfg->getNodeCount());
-  dfsStack.push_back(start);
-  while (!dfsStack.empty()) {
-    StmtValue curStmt = dfsStack.back();
-    dfsStack.pop_back();
-
-    StmtTransitiveResult affectsResult = affectsQuerier
-        .queryFrom(curStmt,
-                   StmtType::Assign);
-    for (const StmtValue &stmt : affectsResult.secondArgVals) {
-      CFGNode nextNode = cfg->toCFGNode(stmt);
-      if (seen.isSet(nextNode)) {
-        continue;
-      }
-      resultOut->add(start, stmt);
-      seen.set(nextNode);
-      dfsStack.push_back(stmt);
-    }
+    runAffectsTDFS(resultHandler, stepGetter, &dfsClosure, startStmt);
   }
 }
 
@@ -230,4 +233,38 @@ bool CFGAffectsTQuerier<ClosureType, typePredicate,
 validateArg(const StmtValue &arg) {
   return cfg->containsStatement(arg)
       && typePredicate(closure, StmtType::Assign, arg);
+}
+
+template<
+    class ClosureType,
+    StmtTypePredicate<ClosureType> typePredicate,
+    ModifiesGetter<ClosureType> modifiesGetter,
+    UsesGetter<ClosureType> usesGetter>
+template <class DFSClosureType, class DFSStepType>
+void CFGAffectsTQuerier<ClosureType, typePredicate,
+                        modifiesGetter, usesGetter>::
+runAffectsTDFS(DFSResultCallback<DFSClosureType> callback,
+               DFSStepGetter<DFSClosureType, DFSStepType> stepGetter,
+               DFSClosureType *dfsClosure,
+               const StmtValue &start) {
+  vector<StmtValue> dfsStack;
+  BitField seen(cfg->getNodeCount());
+  dfsStack.push_back(start);
+  while (!dfsStack.empty()) {
+    StmtValue curStmt = dfsStack.back();
+    dfsStack.pop_back();
+
+    DFSStepType affectsResult = stepGetter(dfsClosure, curStmt);
+    for (const StmtValue &stmt : affectsResult) {
+      CFGNode nextNode = cfg->toCFGNode(stmt);
+      if (seen.isSet(nextNode)) {
+        continue;
+      }
+      if (!callback(dfsClosure, start, stmt)) {
+        return;
+      }
+      seen.set(nextNode);
+      dfsStack.push_back(stmt);
+    }
+  }
 }
