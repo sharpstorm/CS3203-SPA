5e23a62a9801a65ed154b0e3718208e7984f4679
KwanHW <kwanhaowei@gmail.com>
2023-04-06 01:44:22 +0800

    test: override transformer

---
+++ Team30/Code30/src/unit_testing/src/qps/constraints/OverrideTransformer.cpp
@@ -0,0 +1,54 @@
+#include "catch.hpp"
+
+#include "common/Types.h"
+#include "qps/constraints/OverrideTransformer.h"
+
+TEST_CASE("Transform StmtRef") {
+  OverrideTransformer transformer(0);
+
+  // No stmt
+  StmtRef inputRef = StmtRef{StmtType::None, NO_STMT};
+  StmtRef actual = transformer.transformArg(inputRef);
+  REQUIRE(inputRef.getType() == actual.getType());
+  REQUIRE(inputRef.getValue() == actual.getValue());
+
+  transformer = OverrideTransformer(3);
+  StmtRef expected = StmtRef{StmtType::None, 3};
+  inputRef = StmtRef{StmtType::None, 3};
+  actual = transformer.transformArg(inputRef);
+  REQUIRE(expected.getType() == actual.getType());
+  REQUIRE(expected.getValue() == actual.getValue());
+}
+
+TEST_CASE("Transform EntityRef") {
+  OverrideTransformer transformer("");
+
+  // No Ent
+  EntityRef inputRef = EntityRef{EntityType::None, NO_ENT};
+  EntityRef actual = transformer.transformArg(inputRef);
+  REQUIRE(inputRef.getType() == actual.getType());
+  REQUIRE(inputRef.getValue() == actual.getValue());
+
+  // Any other ent except for constant
+  transformer = OverrideTransformer("x");
+  EntityRef expected = EntityRef{EntityType::Variable, "x"};
+  inputRef = EntityRef{EntityType::Variable, "g"};
+  actual = transformer.transformArg(inputRef);
+  REQUIRE(expected.getType() == actual.getType());
+  REQUIRE(expected.getValue() == actual.getValue());
+}
+
+TEST_CASE("Transform EntityRef - Constant case") {
+  OverrideTransformer transformer(0);
+  EntityRef inputRef = EntityRef{EntityType::Constant, NO_ENT};
+  EntityRef actual = transformer.transformArg(inputRef);
+  REQUIRE(inputRef.getType() == actual.getType());
+  REQUIRE(inputRef.getValue() == actual.getValue());
+
+  transformer = OverrideTransformer(5);
+  EntityRef expected = EntityRef{EntityType::Constant, "5"};
+  inputRef = EntityRef{EntityType::Constant, "abc"};
+  actual = transformer.transformArg(inputRef);
+  REQUIRE(expected.getType() == actual.getType());
+  REQUIRE(expected.getValue() == actual.getValue());
+}
